7489038e3c60be7a2478a1c015c4c7d5a5f13c47
Petrus <petrus_ant@hotmail.com>
2021-10-18 03:34:43 -0300

    Check current storage type before setting default value for it

--- src/main/java/br/com/finalcraft/evernifecore/config/settings/ECSettings.java
+++ src/main/java/br/com/finalcraft/evernifecore/config/settings/ECSettings.java
@@ -1,6 +1,12 @@
 package br.com.finalcraft.evernifecore.config.settings;
 
+import br.com.finalcraft.evernifecore.EverNifeCore;
 import br.com.finalcraft.evernifecore.config.ConfigManager;
+import br.com.finalcraft.evernifecore.config.uuids.UUIDsController;
+
+import java.io.File;
+import java.util.Map;
+import java.util.UUID;
 
 public class ECSettings {
 
@@ -9,8 +15,26 @@ public class ECSettings {
 
     public static void initialize(){
 
-        useNamesInsteadOfUUIDToStorePlayerData = ConfigManager.getMainConfig()
-                .getOrSetDefaultValue("Settings.useNamesInsteadOfUUIDToStorePlayerData", false);
+        boolean isUsingStorageWithPlayerName = false;
+
+        if (!ConfigManager.getMainConfig().contains("Settings.useNamesInsteadOfUUIDToStorePlayerData")){//Check if there is any playerdata and if the player data is being store with an UUID
+            for (Map.Entry<UUID, String> entry : UUIDsController.getEntrySet()) {
+                final UUID playerUUID = entry.getKey();
+                final String playerName = entry.getValue();
+                File uuidConfig = new File(EverNifeCore.instance.getDataFolder(), "PlayerData/" + playerUUID + ".yml");
+                if (uuidConfig.exists()){
+                    isUsingStorageWithPlayerName = false;
+                    break;
+                }
+                File nameConfig = new File(EverNifeCore.instance.getDataFolder(), "PlayerData/" + playerName + ".yml");
+                if (nameConfig.exists()){
+                    isUsingStorageWithPlayerName = true;
+                    break;
+                }
+            }
+        }
+
+        useNamesInsteadOfUUIDToStorePlayerData = ConfigManager.getMainConfig().getOrSetDefaultValue("Settings.useNamesInsteadOfUUIDToStorePlayerData", isUsingStorageWithPlayerName);
 
         ZONE_ID_OF_DAY_OF_TODAY = ConfigManager.getMainConfig()
                 .getOrSetDefaultValue("Settings.ZONE_ID_OF_DAY_OF_TODAY", "America/Sao_Paulo");
