4885ca52660b8b2aa37571554a0bdb841d716f3f
Petrus <petrus_ant@hotmail.com>
2021-10-23 19:33:42 -0300

    Add locale for CMDGetName

--- src/main/java/br/com/finalcraft/evernifecore/commands/misc/CMDGetName.java
+++ src/main/java/br/com/finalcraft/evernifecore/commands/misc/CMDGetName.java
@@ -3,30 +3,49 @@ package br.com.finalcraft.evernifecore.commands.misc;
 import br.com.finalcraft.evernifecore.PermissionNodes;
 import br.com.finalcraft.evernifecore.argumento.MultiArgumentos;
 import br.com.finalcraft.evernifecore.commands.finalcmd.annotations.FinalCMD;
-import br.com.finalcraft.evernifecore.config.playerdata.PlayerController;
+import br.com.finalcraft.evernifecore.commands.finalcmd.help.HelpLine;
 import br.com.finalcraft.evernifecore.config.playerdata.PlayerData;
+import br.com.finalcraft.evernifecore.locale.FCLocale;
+import br.com.finalcraft.evernifecore.locale.LocaleMessage;
+import br.com.finalcraft.evernifecore.locale.LocaleType;
+import br.com.finalcraft.evernifecore.util.FCMessageUtil;
 import org.bukkit.command.CommandSender;
 
 import java.util.UUID;
 
 public class CMDGetName {
 
+    @FCLocale(lang = LocaleType.EN_US, text = "§e§l ▶ §7[§2%argumento%§7]§c needs to be a valid UUID!")
+    @FCLocale(lang = LocaleType.PT_BR, text = "§e§l ▶ §7[§2%argumento%§7]§c precisa ser uma UUID válida!")
+    private static LocaleMessage NEEDS_TO_BE_UUID;
+
     @FinalCMD(
             aliases = {"getname", "nameof"},
+            usage = "%label% <UUID>",
             permission = PermissionNodes.EVERNIFECORE_COMMAND_NAMEOF
     )
-    public void onCommand(CommandSender sender, String label, MultiArgumentos argumentos) {
-        if (argumentos.get(0).isEmpty()) sender.sendMessage("/" + label + " <UUID>");
-        try {
-            UUID uuid = UUID.fromString(argumentos.getStringArg(0));
-            PlayerData playerData = PlayerController.getPlayerData(uuid);
-            if (playerData == null){
-                sender.sendMessage("§cNenhum jogador encontrado com a UUID: §e" + uuid);
-            }else {
-                sender.sendMessage("§a [" + uuid + "] pertence ao jogador §e" + playerData.getPlayerName());
-            }
-        }catch (Exception e){
-            sender.sendMessage("§cA UUID inserida é Inválida!");
+    public void onCommand(CommandSender sender, MultiArgumentos argumentos, HelpLine helpLine) {
+        if (argumentos.emptyArgs(0)){
+            helpLine.sendTo(sender);
+            return;
         }
+
+        UUID uuid = argumentos.get(0).getUUID();
+
+        if (uuid == null){
+            NEEDS_TO_BE_UUID
+                    .addPlaceholder("%argumento%", argumentos.get(0))
+                    .send(sender);
+            return;
+        }
+
+        PlayerData playerData = argumentos.get(0).getPlayerData();
+
+        if (playerData == null){
+            FCMessageUtil.playerDataNotFound(sender, argumentos.getStringArg(0));
+            return;
+        }
+
+        sender.sendMessage("§a [" + uuid + "] --> §e" + playerData.getPlayerName());
     }
 }
