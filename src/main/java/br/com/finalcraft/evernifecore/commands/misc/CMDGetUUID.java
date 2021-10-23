282ba04ce0e6b6c7779de4882a89f735ecfc7671
Petrus <petrus_ant@hotmail.com>
2021-10-23 19:34:07 -0300

    few changes on CMDGetUUID

--- src/main/java/br/com/finalcraft/evernifecore/commands/misc/CMDGetUUID.java
+++ src/main/java/br/com/finalcraft/evernifecore/commands/misc/CMDGetUUID.java
@@ -3,6 +3,7 @@ package br.com.finalcraft.evernifecore.commands.misc;
 import br.com.finalcraft.evernifecore.PermissionNodes;
 import br.com.finalcraft.evernifecore.argumento.MultiArgumentos;
 import br.com.finalcraft.evernifecore.commands.finalcmd.annotations.FinalCMD;
+import br.com.finalcraft.evernifecore.commands.finalcmd.help.HelpLine;
 import br.com.finalcraft.evernifecore.config.playerdata.PlayerData;
 import br.com.finalcraft.evernifecore.util.FCMessageUtil;
 import org.bukkit.command.CommandSender;
@@ -11,15 +12,22 @@ public class CMDGetUUID {
 
     @FinalCMD(
             aliases = {"getuuid", "uuidof"},
+            usage = "%label% <playerName>",
             permission = PermissionNodes.EVERNIFECORE_COMMAND_UUIDOF
     )
-    public void onCommand(CommandSender sender, String label, MultiArgumentos argumentos) {
-        if (argumentos.get(0).isEmpty()) sender.sendMessage("/" + label + " <Player>");
+    public void onCommand(CommandSender sender, MultiArgumentos argumentos, HelpLine helpLine) {
+        if (argumentos.emptyArgs(0)){
+            helpLine.sendTo(sender);
+            return;
+        }
+
         PlayerData playerData = argumentos.get(0).getPlayerData();
+
         if (playerData == null){
             FCMessageUtil.playerDataNotFound(sender, argumentos.getStringArg(0));
             return;
         }
-        sender.sendMessage("§a [" + playerData.getUniqueId() + "] pertence ao jogador §e" + playerData.getPlayerName());
+
+        sender.sendMessage("§a [" + playerData.getPlayerName() + "] --> §e" + playerData.getUniqueId());
     }
 }
