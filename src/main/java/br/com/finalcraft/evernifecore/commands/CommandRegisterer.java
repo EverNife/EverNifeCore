89de893101c8763698f6786688d4937cd5fac881
Petrus <petrus_ant@hotmail.com>
2021-10-23 18:19:20 -0300

    Fix double call of CMDBiomeInfo

--- src/main/java/br/com/finalcraft/evernifecore/commands/CommandRegisterer.java
+++ src/main/java/br/com/finalcraft/evernifecore/commands/CommandRegisterer.java
@@ -1,6 +1,7 @@
 package br.com.finalcraft.evernifecore.commands;
 
 import br.com.finalcraft.evernifecore.commands.debug.CMDBiomeInfo;
+import br.com.finalcraft.evernifecore.commands.debug.CMDBlockInfo;
 import br.com.finalcraft.evernifecore.commands.debug.CMDItemInfo;
 import br.com.finalcraft.evernifecore.commands.finalcmd.FinalCMDManager;
 import br.com.finalcraft.evernifecore.commands.misc.CMDCooldown;
@@ -18,7 +19,7 @@ public class CommandRegisterer {
         FinalCMDManager.registerCommand(pluginInstance, CMDGetName.class);
         FinalCMDManager.registerCommand(pluginInstance, CMDGetUUID.class);
         FinalCMDManager.registerCommand(pluginInstance, CMDBiomeInfo.class);
-        FinalCMDManager.registerCommand(pluginInstance, CMDBiomeInfo.class);
+        FinalCMDManager.registerCommand(pluginInstance, CMDBlockInfo.class);
         FinalCMDManager.registerCommand(pluginInstance, CMDItemInfo.class);
 
     }
