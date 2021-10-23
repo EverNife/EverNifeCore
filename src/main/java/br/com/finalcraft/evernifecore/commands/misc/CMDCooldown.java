0c01beef8f9c93506a195ffebc1fe82b87135878
Petrus <petrus_ant@hotmail.com>
2021-10-23 19:45:39 -0300

    Add locale for CMDCooldown

--- src/main/java/br/com/finalcraft/evernifecore/commands/misc/CMDCooldown.java
+++ src/main/java/br/com/finalcraft/evernifecore/commands/misc/CMDCooldown.java
@@ -5,7 +5,12 @@ import br.com.finalcraft.evernifecore.argumento.MultiArgumentos;
 import br.com.finalcraft.evernifecore.commands.finalcmd.annotations.FinalCMD;
 import br.com.finalcraft.evernifecore.commands.finalcmd.help.HelpLine;
 import br.com.finalcraft.evernifecore.config.ConfigManager;
+import br.com.finalcraft.evernifecore.config.playerdata.PlayerData;
 import br.com.finalcraft.evernifecore.cooldown.Cooldown;
+import br.com.finalcraft.evernifecore.locale.FCLocale;
+import br.com.finalcraft.evernifecore.locale.LocaleMessage;
+import br.com.finalcraft.evernifecore.locale.LocaleType;
+import br.com.finalcraft.evernifecore.util.FCMessageUtil;
 import org.bukkit.command.CommandSender;
 
 @FinalCMD(
@@ -14,12 +19,23 @@ import org.bukkit.command.CommandSender;
 )
 public class CMDCooldown {
 
+    @FCLocale(lang = LocaleType.EN_US, text = "§e§l ▶ §cThe cooldown §7[§2%cooldown%§7]§c is not in cooldown!")
+    @FCLocale(lang = LocaleType.PT_BR, text = "§e§l ▶ §cO cooldown §7[§2%cooldown%§7]§c não está em cooldown!")
+    private static LocaleMessage COOLDOWN_NOT_IN_COOLDOWN; //:V
+
+    @FCLocale(lang = LocaleType.EN_US, text = "§e§l ▶ §cThe cooldown §7[§2%cooldown%§7]§c was successfully removed!")
+    @FCLocale(lang = LocaleType.PT_BR, text = "§e§l ▶ §cO cooldown §7[§2%cooldown%§7]§c foi removido com sucesso!")
+    private static LocaleMessage COOLDOWN_REMOVED;
+
     @FinalCMD.SubCMD(
             subcmd = "reset",
             usage = "%name% <CooldownID>",
-            desc = "Recarrega um Cooldown Especifico"
+            locales = {
+                    @FCLocale(lang = LocaleType.EN_US, text = "Reset an specific cooldown!"),
+                    @FCLocale(lang = LocaleType.PT_BR, text = "Reseta um cooldown especifico!")
+            }
     )
-    public void reset(CommandSender sender, String label, MultiArgumentos argumentos, HelpLine helpLine) {
+    public void reset(CommandSender sender, MultiArgumentos argumentos, HelpLine helpLine) {
 
         if (argumentos.emptyArgs(1)){
             helpLine.sendTo(sender);
@@ -27,29 +43,59 @@ public class CMDCooldown {
         }
 
         Cooldown cooldown = Cooldown.of(argumentos.getStringArg(1));
-        if (cooldown == null){
-            sender.sendMessage("§2§l ▶ §aO cooldown " + argumentos.getStringArg(1) + " não existe!");
-            return;
-        }
-
         if (!cooldown.isInCooldown()){
-            sender.sendMessage("§2§l ▶ §aO cooldown " + argumentos.getStringArg(1) + " não está em Cooldown!");
+            COOLDOWN_NOT_IN_COOLDOWN.addPlaceholder("%cooldown%", cooldown.getIdentifier()).send(sender);
             return;
         }
 
         cooldown.stop();
-        sender.sendMessage("§2§l ▶ §aCooldown " + argumentos.getStringArg(1) + " removido com sucesso!");
+        COOLDOWN_REMOVED.addPlaceholder("%cooldown%", cooldown.getIdentifier()).send(sender);
+    }
+
+    @FinalCMD.SubCMD(
+            subcmd = "resetplayer",
+            usage = "%name% <player> <CooldownID>",
+            locales = {
+                    @FCLocale(lang = LocaleType.EN_US, text = "Reset an specific player cooldown!"),
+                    @FCLocale(lang = LocaleType.PT_BR, text = "Reseta um cooldown especifico de um jogador!")
+            }
+    )
+    public void resetPlayerCooldown(CommandSender sender, MultiArgumentos argumentos, HelpLine helpLine) {
+
+        if (argumentos.emptyArgs(1,2)){
+            helpLine.sendTo(sender);
+            return;
+        }
+
+        PlayerData playerData = argumentos.get(1).getPlayerData();
+
+        if (playerData == null){
+            FCMessageUtil.playerDataNotFound(sender, argumentos.getStringArg(1));
+            return;
+        }
+
+        Cooldown cooldown = Cooldown.of(argumentos.getStringArg(2));
+        if (!cooldown.isInCooldown()){
+            COOLDOWN_NOT_IN_COOLDOWN.addPlaceholder("%cooldown%", cooldown.getIdentifier()).send(sender);
+            return;
+        }
+
+        cooldown.stop();
+        COOLDOWN_REMOVED.addPlaceholder("%cooldown%", cooldown.getIdentifier()).send(sender);
         return;
     }
 
 
+
     @FinalCMD.SubCMD(
             subcmd = "reload",
-            desc = "Recarrega todos os Cooldowns"
+            locales = {
+                    @FCLocale(lang = LocaleType.EN_US, text = "Reload all generic cooldown (NonPlayer) !"),
+                    @FCLocale(lang = LocaleType.PT_BR, text = "Recarrega todos os cooldowns genéricos (NonPlayer)!")
+            }
     )
-    public boolean reload(CommandSender sender, String label, MultiArgumentos argumentos) {
+    public void reload(CommandSender sender) {
         ConfigManager.reloadCooldownConfig();
         sender.sendMessage("§2§l ▶ §aTodos os Cooldowns foram recarregados com sucesso!");
-        return true;
     }
 }
