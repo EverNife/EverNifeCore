9184b03faba331a431ac08a63b32b3bfe90df580
Petrus <petrus_ant@hotmail.com>
2021-11-14 03:27:59 -0300

    Add a simple way to clone BossShopPro menu items

--- src/main/java/br/com/finalcraft/evernifecore/integration/BossShopIntegration.java
+++ src/main/java/br/com/finalcraft/evernifecore/integration/BossShopIntegration.java
@@ -1,10 +1,19 @@
 package br.com.finalcraft.evernifecore.integration;
 
+import br.com.finalcraft.evernifecore.util.ReflectionUtil;
+import br.com.finalcraft.evernifecore.util.reflection.FieldAccessor;
 import org.black_ixx.bossshop.BossShop;
 import org.black_ixx.bossshop.api.BossShopAPI;
+import org.black_ixx.bossshop.core.BSBuy;
 import org.black_ixx.bossshop.core.BSShop;
+import org.black_ixx.bossshop.inbuiltaddons.advancedshops.ActionSet;
+import org.black_ixx.bossshop.inbuiltaddons.advancedshops.BSBuyAdvanced;
+import org.black_ixx.bossshop.managers.ClassManager;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+
+import java.util.Map;
 
 public class BossShopIntegration {
 
@@ -33,6 +42,52 @@ public class BossShopIntegration {
 
     public static BossShop getBossShopPlugin(){
         return isPresent() ? bossShopPlugin : null;
+    }
+
+    private static FieldAccessor<Map<ClickType, ActionSet>> ACTIONS_FIELD = ReflectionUtil.getField(BSBuyAdvanced.class, "actions");
+    public static BSBuy cloneBSBuy(BSBuy bsBuy, String newName){
+        BSBuy clone;
+        if (bsBuy instanceof BSBuyAdvanced){
+            BSBuyAdvanced bsBuyAdv = (BSBuyAdvanced) bsBuy;
+            Map<ClickType, ActionSet> actions = ACTIONS_FIELD.get(bsBuyAdv);
+            ACTIONS_FIELD.set(bsBuyAdv, null);//Temporary Remove
+            clone = new BSBuyAdvanced(
+                    bsBuyAdv.getRewardType(ClickType.LEFT),
+                    bsBuyAdv.getPriceType(ClickType.LEFT),
+                    bsBuyAdv.getReward(ClickType.LEFT),
+                    bsBuyAdv.getPrice(ClickType.LEFT),
+                    bsBuyAdv.getMessage(ClickType.LEFT),
+                    bsBuyAdv.getInventoryLocation(),
+                    bsBuyAdv.getExtraPermission(ClickType.LEFT),
+                    newName,
+                    bsBuyAdv.getCondition(),
+                    bsBuyAdv.getInputType(ClickType.LEFT),
+                    bsBuyAdv.getInputText(ClickType.LEFT),
+                    null
+            );
+            ACTIONS_FIELD.set(bsBuyAdv, actions);//Re-Add
+            ACTIONS_FIELD.set(clone, actions);//Add to Clone
+        }else {
+            clone = new BSBuy( //It doesn't matter the ClickType
+                    bsBuy.getRewardType(ClickType.LEFT),
+                    bsBuy.getPriceType(ClickType.LEFT),
+                    bsBuy.getReward(ClickType.LEFT),
+                    bsBuy.getPrice(ClickType.LEFT),
+                    bsBuy.getMessage(ClickType.LEFT),
+                    bsBuy.getInventoryLocation(),
+                    bsBuy.getExtraPermission(ClickType.LEFT),
+                    newName,
+                    bsBuy.getCondition(),
+                    bsBuy.getInputType(ClickType.LEFT),
+                    bsBuy.getInputText(ClickType.LEFT)
+            );
+        }
+        clone.setItem(bsBuy.getItem().clone(), bsBuy.isItemFix());
+        return clone;
+    }
+
+    public static ClassManager getClassManager(){
+        return ClassManager.manager;
     }
 
 }
--- src/main/java/br/com/finalcraft/evernifecore/listeners/bossshop/BossShopListener.java
+++ src/main/java/br/com/finalcraft/evernifecore/listeners/bossshop/BossShopListener.java
@@ -1,18 +1,75 @@
 package br.com.finalcraft.evernifecore.listeners.bossshop;
 
+import br.com.finalcraft.evernifecore.EverNifeCore;
+import br.com.finalcraft.evernifecore.integration.BossShopIntegration;
 import br.com.finalcraft.evernifecore.integration.datapart.ItemDataPartNBT;
 import br.com.finalcraft.evernifecore.listeners.base.ECListener;
+import org.black_ixx.bossshop.core.BSBuy;
+import org.black_ixx.bossshop.events.BSLoadShopItemsEvent;
 import org.black_ixx.bossshop.events.BSRegisterTypesEvent;
+import org.black_ixx.bossshop.inbuiltaddons.advancedshops.BSBuyAdvanced;
+import org.black_ixx.bossshop.managers.config.BSConfigShop;
 import org.black_ixx.bossshop.managers.item.ItemDataPart;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BossShopListener implements ECListener {
 
     private final ItemDataPartNBT DATAPART_NBT = new ItemDataPartNBT();
 
     @EventHandler
-    public void onRegisterTypesEvent(BSRegisterTypesEvent event) {
+    public void onRegisterTypes(BSRegisterTypesEvent event) {
         ItemDataPart.registerType(DATAPART_NBT);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onBSLoadShopItem(BSLoadShopItemsEvent event) {
+
+        if (!(event.getShop() instanceof BSConfigShop)){
+            return;
+        }
+
+        BSConfigShop configShop = (BSConfigShop) event.getShop();
+
+        ConfigurationSection config = configShop.getConfigurationSection();
+        
+        List<BSBuy> allItems = new ArrayList<>(configShop.getItems()); //Clone to prevent ConcurrentModificationException
+
+        for (BSBuy bsBuy : allItems) {
+
+            if (!(bsBuy instanceof BSBuy) && !(bsBuy instanceof BSBuyAdvanced)){ //Ignore other types of BSBuy (custom plugins?)
+                continue;
+            }
+
+            ConfigurationSection itemSection = config.getConfigurationSection("shop." + bsBuy.getName());
+
+            if (itemSection == null){
+                continue; //If there is no config, we can't do anything
+            }
+
+            if (!itemSection.contains("CopyToLocations")) {
+                continue; //This item does not need to be cloned
+            }
+
+            try {
+                List<Integer> integerList = itemSection.getStringList("CopyToLocations").stream().map(Integer::parseInt).collect(Collectors.toList());
+
+                for (Integer integer : integerList) {
+                    BSBuy bsBuyClone = BossShopIntegration.cloneBSBuy(bsBuy, bsBuy.getName() + "$C_" + integer);
+                    bsBuyClone.setInventoryLocation(integer - 1);
+                    configShop.addShopItem(bsBuyClone, bsBuyClone.getItem(),BossShopIntegration.getClassManager());
+                }
+
+            }catch (Exception e){
+                EverNifeCore.warning("Failed to load [shop." + bsBuy.getName() + ".CopyToLocations] from [" + configShop.getConfig().getName() +  "]");
+                e.printStackTrace();
+            }
+        }
     }
 
 }
