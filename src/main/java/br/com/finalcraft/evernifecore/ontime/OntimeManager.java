0c7d875bfabb749345270d2cd7b5f0242a8939a4
Petrus <petrus_ant@hotmail.com>
2021-11-20 05:58:54 -0300

    PLAY_ONE_TICK Statistic is not present anymore on modern api.

--- src/main/java/br/com/finalcraft/evernifecore/ontime/OntimeManager.java
+++ src/main/java/br/com/finalcraft/evernifecore/ontime/OntimeManager.java
@@ -1,11 +1,17 @@
 package br.com.finalcraft.evernifecore.ontime;
 
 import br.com.finalcraft.evernifecore.config.playerdata.PlayerData;
+import br.com.finalcraft.evernifecore.version.MCVersion;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.Statistic;
 
 public class OntimeManager {
+
+    private static final Statistic PLAY_ONE_TICK =
+            MCVersion.isCurrentEqualOrHigher(MCVersion.v1_13_R1) ?
+                    Statistic.PLAY_ONE_MINUTE :
+                    Statistic.valueOf("PLAY_ONE_TICK");
 
     private static IOntimeProvider ontimeProvider = new IOntimeProvider() {
         @Override
@@ -13,7 +19,7 @@ public class OntimeManager {
             OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(playerData.getUniqueId());
 
             if (offlinePlayer.isOnline()){
-                return offlinePlayer.getPlayer().getStatistic(Statistic.PLAY_ONE_TICK) * 50;
+                return offlinePlayer.getPlayer().getStatistic(PLAY_ONE_TICK) * 50;
             }
 
             return 0;
