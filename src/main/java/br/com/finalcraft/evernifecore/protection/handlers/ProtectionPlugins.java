404bbf82362833116cabb7825f777b7a78140827
Petrus <petrus_ant@hotmail.com>
2021-12-03 04:17:28 -0300

    Missed a map in here

--- src/main/java/br/com/finalcraft/evernifecore/protection/handlers/ProtectionPlugins.java
+++ src/main/java/br/com/finalcraft/evernifecore/protection/handlers/ProtectionPlugins.java
@@ -8,15 +8,13 @@ import br.com.finalcraft.evernifecore.util.ReflectionUtil;
 import br.com.finalcraft.evernifecore.util.reflection.MethodInvoker;
 import org.bukkit.Bukkit;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public enum ProtectionPlugins {
-
 	GriefPreventionPlus(GriefPreventionPlusHandler.class),
-	WorldGuard(WorldGuardHandler.class, () -> { //This will prevent Boostrap CLI INIT ERRORs when WorldGuard is not present
-		MethodInvoker invoker = ReflectionUtil.getMethod(ProtectionWorldGuard.class, "initialize");
-		invoker.invoke(null);
-	}),
+	WorldGuard(WorldGuardHandler.class),
 	;
 
 	public static void initialize(){
@@ -25,30 +23,23 @@ public enum ProtectionPlugins {
 				try {
 					EverNifeCore.info("[ProtectionPlugins] Loading protection plugin: " + protectionPlugin.name());
 					protectionPlugin.createHandler();
-					if (protectionPlugin.isEnabled()){
-						protectionPlugin.onPostHandler.run();
-					}
-				}catch (Throwable e){ //Some REQUIRED Plugin is not present
-					EverNifeCore.warning("[ProtectionPlugins] Protection plugin [" + protectionPlugin.name() + "] was not found! ClassNotFound[" +  e.getMessage() +  "]");
+				}catch (Throwable e){
+					EverNifeCore.warning("[ProtectionPlugins] Protection plugin [" + protectionPlugin.name() + "] was found but i was not able to integrate with it!");
+					e.printStackTrace();
 				}
 			}
 		}
+		generateHandlersList();
 	}
 
 	private final Class<? extends ProtectionHandler> clazz;
 	private ProtectionHandler handler;
 	private boolean enabled = false;
-	private Runnable onPostHandler = null;
 
 	private static ProtectionHandler[] handlersList={};
 
 	ProtectionPlugins(Class<? extends ProtectionHandler> clazz) {
 		this.clazz=clazz;
-	}
-
-	ProtectionPlugins(Class<? extends ProtectionHandler> clazz, Runnable onPostHandler) {
-		this.clazz = clazz;
-		this.onPostHandler = onPostHandler;
 	}
 
 	public ProtectionHandler getHandler() {
@@ -59,21 +50,20 @@ public enum ProtectionPlugins {
 		return enabled;
 	}
 
-	public void createHandler() throws InstantiationException, IllegalAccessException {
-		this.handler = this.clazz.newInstance();
+	public void createHandler() throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+		this.handler = this.clazz.getDeclaredConstructor().newInstance();
 		this.enabled = true;
-		generateHandlersList();
 	}
 
 	public void removeHandler() {
 		this.handler = null;
 		this.enabled = false;
-		generateHandlersList();
 	}
 
 	protected static void generateHandlersList() {
 		handlersList = Arrays.stream(ProtectionPlugins.values())
 				.filter(ProtectionPlugins::isEnabled)
+				.map(protectedPlugin -> protectedPlugin.getHandler())
 				.toArray(ProtectionHandler[]::new);
 	}
 	
