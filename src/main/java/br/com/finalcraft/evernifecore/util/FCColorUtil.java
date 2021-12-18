7f59268e49407c6ce23a43c780ae86c28bb7eeff
Petrus <petrus_ant@hotmail.com>
2021-12-18 16:47:11 -0300

    Add easy way to colorify and stripColors

--- src/main/java/br/com/finalcraft/evernifecore/util/FCColorUtil.java
+++ src/main/java/br/com/finalcraft/evernifecore/util/FCColorUtil.java
@@ -1,6 +1,7 @@
 package br.com.finalcraft.evernifecore.util;
 
 import org.bukkit.ChatColor;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,29 @@ public class FCColorUtil {
             }
         }
         return colors.get(FCBukkitUtil.getRandom().nextInt(colors.size()));
+    }
+
+    public static String colorfy(String text){
+        return ChatColor.translateAlternateColorCodes('&',text);
+    }
+
+    public static List<String> colorfy(List<String> text){
+        for (int i = 0; i < text.size(); i++) {
+            text.set(i, colorfy(text.get(i)));
+        }
+        return text;
+    }
+
+    public static String stripColor(@Nullable final String input){
+        return ChatColor.stripColor(input);
+    }
+
+    public static List<String> stripColor(@Nullable final List<String> text){
+        if (text == null) return null;
+        for (int i = 0; i < text.size(); i++) {
+            text.set(i, stripColor(text.get(i)));
+        }
+        return text;
     }
 
 }
