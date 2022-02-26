3fcfcfa2d698944ecd64997e25570103b3a8c4ff
Petrus <petrus_ant@hotmail.com>
2022-02-26 05:27:28 -0300

    Add ItemStack (heldItem) as possible arg

--- src/main/java/br/com/finalcraft/evernifecore/commands/finalcmd/executor/ExecutorInterpreter.java
+++ src/main/java/br/com/finalcraft/evernifecore/commands/finalcmd/executor/ExecutorInterpreter.java
@@ -7,10 +7,13 @@ import br.com.finalcraft.evernifecore.commands.finalcmd.help.HelpLine;
 import br.com.finalcraft.evernifecore.config.playerdata.PDSection;
 import br.com.finalcraft.evernifecore.config.playerdata.PlayerController;
 import br.com.finalcraft.evernifecore.config.playerdata.PlayerData;
+import br.com.finalcraft.evernifecore.util.FCBukkitUtil;
+import br.com.finalcraft.evernifecore.util.FCMessageUtil;
 import br.com.finalcraft.evernifecore.util.ReflectionUtil;
 import br.com.finalcraft.evernifecore.util.commons.Tuple;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -28,6 +31,10 @@ public class ExecutorInterpreter {
             CMDParameterType.of(MultiArgumentos.class).build(),
             CMDParameterType.of(HelpContext.class).build(),
             CMDParameterType.of(HelpLine.class).build(),
+
+            CMDParameterType.of(ItemStack.class)
+                    .setOnlyPlayer(true)
+                    .build(),
 
             CMDParameterType.of(Player.class)
                     .setOnlyPlayer(true)
@@ -56,7 +63,7 @@ public class ExecutorInterpreter {
         for (CMDParameterType parameterType : ALLOWED_CLASSES) {
             Integer index = ReflectionUtil.getArgIndex(method, parameterType.getClazz(), parameterType.isCheckExtends());
             if (index != null){
-                Tuple<CMDParameterType, Class> tuple = Tuple.from(parameterType, ReflectionUtil.getArgAtIndex(method, index));
+                Tuple<CMDParameterType, Class> tuple = Tuple.of(parameterType, ReflectionUtil.getArgAtIndex(method, index));
 
                 methodArguments.put(index, tuple);
 
@@ -89,6 +96,13 @@ public class ExecutorInterpreter {
             if (parameterType.getClazz() == Player.class) { theArgs[index] = sender; continue; }
             if (parameterType.getClazz() == PlayerData.class) { theArgs[index] = PlayerController.getPlayerData((Player) sender); continue; }
             if (parameterType.getClazz() == PDSection.class) { theArgs[index] = PlayerController.getPDSection((Player) sender, parameterClass); continue; }
+            if (parameterType.getClazz() == ItemStack.class) {
+                theArgs[index] = FCBukkitUtil.getPlayersHeldItem((Player) sender);
+                if (theArgs[index] == null){
+                    FCMessageUtil.needsToBeHoldingItem(sender);
+                    return;
+                }
+            }
 
             for (Object possibleArg : possibleArgs) {
                 if (parameterType.isCheckExtends()){
