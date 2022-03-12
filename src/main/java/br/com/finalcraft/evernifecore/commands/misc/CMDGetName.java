28b4fdbc779841d934fdfcd3876685d6c17082ef
Petrus <petrus_ant@hotmail.com>
2022-03-12 18:36:54 -0300

    Rework CMDGetNam and CMDGetUUID to have tab completion

--- src/main/java/br/com/finalcraft/evernifecore/commands/misc/CMDGetName.java
+++ src/main/java/br/com/finalcraft/evernifecore/commands/misc/CMDGetName.java
@@ -1,51 +1,23 @@
 package br.com.finalcraft.evernifecore.commands.misc;
 
 import br.com.finalcraft.evernifecore.PermissionNodes;
-import br.com.finalcraft.evernifecore.argumento.MultiArgumentos;
+import br.com.finalcraft.evernifecore.commands.finalcmd.annotations.Arg;
 import br.com.finalcraft.evernifecore.commands.finalcmd.annotations.FinalCMD;
-import br.com.finalcraft.evernifecore.commands.finalcmd.help.HelpLine;
+import br.com.finalcraft.evernifecore.config.playerdata.PlayerController;
 import br.com.finalcraft.evernifecore.config.playerdata.PlayerData;
-import br.com.finalcraft.evernifecore.locale.FCLocale;
-import br.com.finalcraft.evernifecore.locale.LocaleMessage;
-import br.com.finalcraft.evernifecore.locale.LocaleType;
-import br.com.finalcraft.evernifecore.util.FCMessageUtil;
 import org.bukkit.command.CommandSender;
 
 import java.util.UUID;
 
 public class CMDGetName {
 
-    @FCLocale(lang = LocaleType.EN_US, text = "§e§l ▶ §7[§2%argumento%§7]§c needs to be a valid UUID!")
-    @FCLocale(lang = LocaleType.PT_BR, text = "§e§l ▶ §7[§2%argumento%§7]§c precisa ser uma UUID válida!")
-    private static LocaleMessage NEEDS_TO_BE_UUID;
-
     @FinalCMD(
             aliases = {"getname", "nameof"},
-            usage = "%label% <UUID>",
             permission = PermissionNodes.EVERNIFECORE_COMMAND_NAMEOF
     )
-    public void onCommand(CommandSender sender, MultiArgumentos argumentos, HelpLine helpLine) {
-        if (argumentos.emptyArgs(0)){
-            helpLine.sendTo(sender);
-            return;
-        }
-
-        UUID uuid = argumentos.get(0).getUUID();
-
-        if (uuid == null){
-            NEEDS_TO_BE_UUID
-                    .addPlaceholder("%argumento%", argumentos.get(0))
-                    .send(sender);
-            return;
-        }
-
-        PlayerData playerData = argumentos.get(0).getPlayerData();
-
-        if (playerData == null){
-            FCMessageUtil.playerDataNotFound(sender, argumentos.getStringArg(0));
-            return;
-        }
-
-        sender.sendMessage("§a [" + uuid + "] --> §e" + playerData.getPlayerName());
+    public void onCommand(CommandSender sender, @Arg(name = "<PlayerUUID>") UUID playerUUID) {
+        PlayerData playerData = PlayerController.getPlayerData(playerUUID);
+        sender.sendMessage("§a [" + playerData.getPlayerName() + "] --> §e" + playerData.getUniqueId());
     }
+
 }
--- src/main/java/br/com/finalcraft/evernifecore/commands/misc/CMDGetUUID.java
+++ src/main/java/br/com/finalcraft/evernifecore/commands/misc/CMDGetUUID.java
@@ -1,33 +1,19 @@
 package br.com.finalcraft.evernifecore.commands.misc;
 
 import br.com.finalcraft.evernifecore.PermissionNodes;
-import br.com.finalcraft.evernifecore.argumento.MultiArgumentos;
+import br.com.finalcraft.evernifecore.commands.finalcmd.annotations.Arg;
 import br.com.finalcraft.evernifecore.commands.finalcmd.annotations.FinalCMD;
-import br.com.finalcraft.evernifecore.commands.finalcmd.help.HelpLine;
 import br.com.finalcraft.evernifecore.config.playerdata.PlayerData;
-import br.com.finalcraft.evernifecore.util.FCMessageUtil;
 import org.bukkit.command.CommandSender;
 
 public class CMDGetUUID {
 
     @FinalCMD(
             aliases = {"getuuid", "uuidof"},
-            usage = "%label% <playerName>",
             permission = PermissionNodes.EVERNIFECORE_COMMAND_UUIDOF
     )
-    public void onCommand(CommandSender sender, MultiArgumentos argumentos, HelpLine helpLine) {
-        if (argumentos.emptyArgs(0)){
-            helpLine.sendTo(sender);
-            return;
-        }
-
-        PlayerData playerData = argumentos.get(0).getPlayerData();
-
-        if (playerData == null){
-            FCMessageUtil.playerDataNotFound(sender, argumentos.getStringArg(0));
-            return;
-        }
-
+    public void onCommand(CommandSender sender, @Arg(name = "<PlayerUUID>") PlayerData playerData) {
         sender.sendMessage("§a [" + playerData.getPlayerName() + "] --> §e" + playerData.getUniqueId());
     }
+
 }
