2ad87b416d851412e83c15055fc4532696277e8f
Petrus <petrus_ant@hotmail.com>
2022-03-12 22:16:34 -0300

    Load Locale on ArgParsers as well

--- src/main/java/br/com/finalcraft/evernifecore/commands/finalcmd/argument/ArgParserManager.java
+++ src/main/java/br/com/finalcraft/evernifecore/commands/finalcmd/argument/ArgParserManager.java
@@ -1,7 +1,10 @@
 package br.com.finalcraft.evernifecore.commands.finalcmd.argument;
 
+import br.com.finalcraft.evernifecore.locale.FCLocaleManager;
 import br.com.finalcraft.evernifecore.util.commons.Tuple;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,12 +18,18 @@ public class ArgParserManager {
 
     public static void addGlobalParser(Class clazz, Class<? extends ArgParser> parser){
         GLOBAL_CONTEXT_PARSER.getParsers().add(Tuple.of(clazz, parser));
+
+        Plugin plugin = JavaPlugin.getProvidingPlugin(parser);
+        FCLocaleManager.loadLocale(plugin, parser);
     }
 
     public static void addPluginParser(Plugin plugin, Class clazz, Class<? extends ArgParser> parser){
         PARSER_MAP.computeIfAbsent(plugin.getName(), s -> new ParserContext())
                 .getParsers()
                 .add(Tuple.of(clazz, parser));
+
+        Plugin parserPlugin = JavaPlugin.getProvidingPlugin(parser); //Not always the same as the plugin adding it
+        FCLocaleManager.loadLocale(parserPlugin, parser);
     }
 
     public static Class<? extends ArgParser> getParser(Plugin plugin, Class argument){
