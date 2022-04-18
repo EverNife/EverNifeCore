4b7c87518a01d51eb08abb90ba2e3a78f33ab554
Pétrus Pradella <petrus_ant@hotmail.com>
2022-04-18 20:02:14 -0300

    Add way to get PDSection form ECPlayerDataEvents

--- src/main/java/br/com/finalcraft/evernifecore/api/events/base/ECPlayerDataEvent.java
+++ src/main/java/br/com/finalcraft/evernifecore/api/events/base/ECPlayerDataEvent.java
@@ -1,5 +1,6 @@
 package br.com.finalcraft.evernifecore.api.events.base;
 
+import br.com.finalcraft.evernifecore.config.playerdata.PDSection;
 import br.com.finalcraft.evernifecore.config.playerdata.PlayerData;
 import org.bukkit.entity.Player;
 
@@ -13,6 +14,10 @@ public class ECPlayerDataEvent extends ECBaseEvent {
 
     public PlayerData getPlayerData() {
         return playerData;
+    }
+
+    public <T extends PDSection> T getPDSection(Class<T> pdSectionClass) {
+        return playerData.getPDSection(pdSectionClass);
     }
 
     public Player getPlayer(){
