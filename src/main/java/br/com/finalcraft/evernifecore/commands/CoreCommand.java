6e2facead438c551a3c8e4dfd37967fc007679f3
Pétrus Pradella <petrus_ant@hotmail.com>
2022-04-25 20:49:51 -0300

    Add command info to show info of all ECPlugins installed, and if they have any update available.

--- src/main/java/br/com/finalcraft/evernifecore/commands/CoreCommand.java
+++ src/main/java/br/com/finalcraft/evernifecore/commands/CoreCommand.java
@@ -12,11 +12,43 @@ import br.com.finalcraft.evernifecore.locale.LocaleType;
 import br.com.finalcraft.evernifecore.util.pageviwer.PageViewer;
 import org.bukkit.command.CommandSender;
 
+import java.util.ArrayList;
+
 @FinalCMD(
         aliases = {"evernifecore","ecore"},
         permission = PermissionNodes.EVERNIFECORE_COMMAND_RELOAD
 )
 public class CoreCommand {
+
+    @FinalCMD.SubCMD(
+            subcmd = "info",
+            locales = {
+                    @FCLocale(lang = LocaleType.EN_US, text = "Show info of the EverNifeCore and its addons!"),
+                    @FCLocale(lang = LocaleType.PT_BR, text = "Mostra informações do EverNifeCore e de seus addons!")
+            }
+    )
+    public void info(CommandSender sender, @Arg(name = "[page]", context = "[1:*]") Integer page){
+        PageViewer.builder(
+                        () -> new ArrayList<>(ECPluginManager.getECPluginsMap().values()),
+                        ecPlugin -> ecPlugin.getPlugin().getName()
+                ).setFormatLine(
+                        FancyText.of("§7# %number%: §e§l◆ §a %value% §7§o(%version%)").setHoverText("%plugin_info%")
+                                .append("%can_update%").setHoverText("§aClique to go to DownloadLink").setOpenLinkAction("%update_link%")
+                )
+                .addPlaceholder("%version%", ecPlugin -> ecPlugin.getPlugin().getDescription().getVersion())
+                .addPlaceholder("%can_update%", ecPlugin -> ecPlugin.hasUpdate() ? "§b  [Update]" : "")
+                .addPlaceholder("%update_link%", ecPlugin -> ecPlugin.hasUpdate() ? ecPlugin.getUpdateLink() : "")
+                .addPlaceholder("%plugin_info%", ecPlugin -> {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append("\n§d ▲ Name: §a" + ecPlugin.getPlugin().getName());
+                    stringBuilder.append("\n§d ▲ Version: §a" + ecPlugin.getPlugin().getDescription().getVersion());
+                    stringBuilder.append("\n\n§d ▲ Is Up To Date: " + (ecPlugin.hasUpdate() ? "§b" : "§c") + !ecPlugin.hasUpdate());
+                    stringBuilder.append("\n");
+                    return stringBuilder.toString();
+                })
+                .build()
+                .send(page, sender);
+    }
 
     @FinalCMD.SubCMD(
             subcmd = "reload",
