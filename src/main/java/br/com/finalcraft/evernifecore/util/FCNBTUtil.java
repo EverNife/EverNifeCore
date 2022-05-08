7a1cdbf7c01da67ad904d73e9888fd5ce6bb1747
Pétrus Pradella <petrus_ant@hotmail.com>
2022-05-08 19:29:55 -0300

    Add a few annotations to FCNBTUtil

--- src/main/java/br/com/finalcraft/evernifecore/util/FCNBTUtil.java
+++ src/main/java/br/com/finalcraft/evernifecore/util/FCNBTUtil.java
@@ -8,37 +8,38 @@ import org.bukkit.block.BlockState;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
 
 public class FCNBTUtil {
 
-    public static NBTPlayer getFrom(Player player){
+    public static @NotNull NBTPlayer getFrom(Player player){
         return new NBTPlayer(player);
     }
 
-    public static NBTEntity getFrom(Entity entity){
+    public static @NotNull NBTEntity getFrom(Entity entity){
         return new NBTEntity(entity);
     }
 
-    public static NBTItem getFrom(ItemStack itemStack){
+    public static @NotNull NBTItem getFrom(ItemStack itemStack){
         return new NBTItem(itemStack, true);
     }
 
-    public static NBTBlock getFrom(Block block){
+    public static @NotNull NBTBlock getFrom(Block block){
         return new NBTBlock(block);
     }
 
-    public static NBTTileEntity getFrom(BlockState tile){
+    public static @NotNull NBTTileEntity getFrom(BlockState tile){
         return new NBTTileEntity(tile);
     }
 
-    public static NBTChunk getFrom(Chunk chunk){
+    public static @NotNull NBTChunk getFrom(Chunk chunk){
         return new NBTChunk(chunk);
     }
 
-    public static NBTFile getFrom(File file){
+    public static @NotNull NBTFile getFrom(File file){
         try {
             return new NBTFile(file);
         } catch (IOException e) {
