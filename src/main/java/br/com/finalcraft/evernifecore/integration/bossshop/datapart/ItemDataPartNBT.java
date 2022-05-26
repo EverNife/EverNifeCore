cad30f3b22b08aea576cf66db9942f857e4da4f7
Pétrus Pradella <petrus_ant@hotmail.com>
2022-05-26 15:24:31 -0300

    BossShopPro integration should follow EverNifeCore pattern

--- src/main/java/br/com/finalcraft/evernifecore/integration/bossshop/datapart/ItemDataPartNBT.java
+++ src/main/java/br/com/finalcraft/evernifecore/integration/bossshop/datapart/ItemDataPartNBT.java
@@ -1,6 +1,5 @@
 package br.com.finalcraft.evernifecore.integration.bossshop.datapart;
 
-import br.com.finalcraft.evernifecore.nms.util.NMSUtils;
 import org.black_ixx.bossshop.core.BSBuy;
 import org.black_ixx.bossshop.managers.item.ItemDataPart;
 import org.bukkit.entity.Player;
@@ -12,37 +11,32 @@ public class ItemDataPartNBT extends ItemDataPart {
 
     @Override
     public ItemStack transform(ItemStack item, String used_name, String argument) {
-        ItemStack override = NMSUtils.get().validateItemStackHandle(item);
-        NMSUtils.get().applyNBTFromString(override, argument.trim());
-        return override;
+        return br.com.finalcraft.evernifecore.itemdatapart.ItemDataPart.NBT.transform(item, used_name, argument);
     }
 
     @Override
     public int getPriority() {
-        return 5; // AFTER "PRIORITY_MOST_EARLY"
+        return br.com.finalcraft.evernifecore.itemdatapart.ItemDataPart.NBT.getPriority();
     }
 
     @Override
     public boolean removeSpaces() {
-        return false;
+        return br.com.finalcraft.evernifecore.itemdatapart.ItemDataPart.NBT.removeSpaces();
     }
 
     @Override
     public String[] createNames() {
-        return new String[]{"nbt", "rawnbt"};
+        return br.com.finalcraft.evernifecore.itemdatapart.ItemDataPart.NBT.createNames();
     }
 
     @Override
     public List<String> read(ItemStack i, List<String> output) {
-        if (NMSUtils.get().hasNBTTagCompound(i)){
-            output.add("nbt: '" + NMSUtils.get().getNBTtoString(i) + "'");
-        }
-        return output;
+        return br.com.finalcraft.evernifecore.itemdatapart.ItemDataPart.NBT.read(i, output);
     }
 
     @Override
     public boolean isSimilar(ItemStack shop_item, ItemStack player_item, BSBuy buy, Player p) {
-        return true; //To expensive to check
+        return true; //Too expensive to check
     }
 
 }
