87c1060dde9698083dc2ba110be94de989a5398d
Pétrus Pradella <petrus_ant@hotmail.com>
2022-07-28 05:36:52 -0300

    The builder does not need the dataPart()

--- src/main/java/br/com/finalcraft/evernifecore/gui/layout/LayoutIconBuilder.java
+++ src/main/java/br/com/finalcraft/evernifecore/gui/layout/LayoutIconBuilder.java
@@ -61,6 +61,6 @@ public class LayoutIconBuilder {
     }
 
     public static LayoutIconBuilder of(LayoutIcon layoutIcon){
-        return new LayoutIconBuilder(layoutIcon.getItemStack().clone(), layoutIcon.getSlot(), layoutIcon.isBackground(), layoutIcon.getPermission(), layoutIcon.getDataPart());
+        return new LayoutIconBuilder(layoutIcon.getItemStack().clone(), layoutIcon.getSlot(), layoutIcon.isBackground(), layoutIcon.getPermission(), null);
     }
 }
