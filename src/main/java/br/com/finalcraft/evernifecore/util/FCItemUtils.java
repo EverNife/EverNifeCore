3e5409c36265f583e2989f8f14eadffd49c1ae0f
Pétrus Pradella <petrus_ant@hotmail.com>
2022-11-05 16:43:23 -0300

    Remove several deprecated code form FCItemUtils

--- src/main/java/br/com/finalcraft/evernifecore/util/FCItemUtils.java
+++ src/main/java/br/com/finalcraft/evernifecore/util/FCItemUtils.java
@@ -44,63 +44,6 @@ public class FCItemUtils {
         itemStack.setItemMeta(itemMeta);
     }
 
-    @Deprecated
-    public static ItemStack getCustomItem(Material material, int amount, byte meta, @Nullable String displayName, @Nullable List<String> loreLines) {
-        ItemStack itemStack = new ItemStack(material, amount, meta);
-        if (displayName != null || loreLines != null){
-            ItemMeta itemMeta = itemStack.getItemMeta();
-            if (displayName != null) itemMeta.setDisplayName(displayName);
-            if (loreLines != null) itemMeta.setLore(loreLines);
-            itemStack.setItemMeta(itemMeta);
-        }
-        return itemStack;
-    }
-
-    @Deprecated
-    public static ItemStack getCustomItem(Material material, int amount, @Nullable String displayName, @Nullable List<String> loreLines) {
-        return getCustomItem(material, amount, (byte) 0, displayName, loreLines);
-    }
-
-    @Deprecated
-    public static ItemStack getCustomItem(Material material, int amount, @Nullable String displayName) {
-        return getCustomItem(material, amount, (byte) 0, displayName, null);
-    }
-
-    @Deprecated
-    public static ItemStack getCustomItem(Material material, int amount, @Nullable List<String> loreLines) {
-        return getCustomItem(material, amount, (byte) 0, null, loreLines);
-    }
-
-    @Deprecated
-    public static ItemStack getCustomItem(Material material, Integer amount, String name) {
-        ItemStack itemStack = new ItemStack(material, amount);
-        ItemMeta itemMeta = itemStack.getItemMeta();
-        itemMeta.setDisplayName(name);
-        itemStack.setItemMeta(itemMeta);
-
-        return itemStack;
-    }
-
-    @Deprecated
-    public static ItemStack getCustomItem(ItemStack itemStack, List<String> loreLines) {
-        ItemStack clone = itemStack.clone();
-        ItemMeta itemMeta = clone.getItemMeta();
-        itemMeta.setLore(loreLines);
-        clone.setItemMeta(itemMeta);
-
-        return clone;
-    }
-
-    @Deprecated
-    public static ItemStack getCustomItem(ItemStack itemStack, String name) {
-        ItemStack clone = itemStack.clone();
-        ItemMeta itemMeta = clone.getItemMeta();
-        itemMeta.setDisplayName(name);
-        clone.setItemMeta(itemMeta);
-
-        return clone;
-    }
-
     public static String getBukkitIdentifier(ItemStack itemStack){
         int durability = itemStack.getDurability();
         return itemStack.getType().name() + (durability == 0 ? "" : ":" + durability);
