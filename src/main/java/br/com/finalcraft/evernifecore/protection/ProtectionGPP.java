fd5ea34a1979c21c06128d741c6b7f0ed42f98c6
Pétrus Pradella <petrus_ant@hotmail.com>
2022-12-04 05:51:35 -0300

    Refactor ProtectionGPP

--- src/main/java/br/com/finalcraft/evernifecore/protection/ProtectionGPP.java
+++ src/main/java/br/com/finalcraft/evernifecore/protection/ProtectionGPP.java
@@ -2,44 +2,38 @@ package br.com.finalcraft.evernifecore.protection;
 
 import net.kaikk.mc.gpp.Claim;
 import net.kaikk.mc.gpp.GriefPreventionPlus;
-import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 public class ProtectionGPP {
 
-    public static boolean canPvP(String playerName) {
-        Player player = Bukkit.getPlayer(playerName);
-        return canPvP(player);
+    public static boolean canBreak(Player player, Block block) {
+        Claim claimAtBlock = getClaimAt(block.getLocation());
+        return claimAtBlock == null || claimAtBlock.canBreak(player, block.getType()) == null;
     }
 
-    public static boolean canPvP(Player player) {
-        Claim claim = GriefPreventionPlus.getInstance().getDataStore().getClaimAt(player.getLocation(), true, null);
-        return claim == null;
+    public static boolean canBuild(Player player, Block block) {
+        Claim claimAtBlock = getClaimAt(block.getLocation());
+        return claimAtBlock == null || claimAtBlock.canBuild(player, block.getType()) == null;
     }
 
-    public static boolean isProtected(Player player, Block b) {
-        Claim claim = GriefPreventionPlus.getInstance().getDataStore().getClaimAt(b.getLocation(), true, null);
-        if (claim != null) {
-            return claim.canBreak(player, b.getType()) == null;
-        } else {
-            return false;
-        }
+    public static Claim getClaimAt(Location location){
+        return GriefPreventionPlus.getInstance().getDataStore().getClaimAt(location);
     }
 
     public static boolean isInsideWilderness(Player player){
-        Claim claim = GriefPreventionPlus.getInstance().getDataStore().getClaimAt(player.getLocation());
-        return claim == null;
-    }
-
-    public static boolean isInsideSelfClaimOrWilderness(Player player){
-        Claim claim = GriefPreventionPlus.getInstance().getDataStore().getClaimAt(player.getLocation());
-        return claim == null || claim.getOwnerID().toString().equalsIgnoreCase(player.getUniqueId().toString());
+        return getClaimAt(player.getLocation()) == null;
     }
 
     public static boolean isInsideSelfClaim(Player player){
-        Claim claim = GriefPreventionPlus.getInstance().getDataStore().getClaimAt(player.getLocation());
-        return claim != null && claim.getOwnerID().toString().equalsIgnoreCase(player.getUniqueId().toString());
+        Claim claim = getClaimAt(player.getLocation());
+        return claim != null && claim.getOwnerID().equals(player.getUniqueId());
+    }
+
+    public static boolean isInsideSelfClaimOrWilderness(Player player){
+        Claim claim = getClaimAt(player.getLocation());
+        return claim == null || claim.getOwnerID().equals(player.getUniqueId());
     }
 
 }
