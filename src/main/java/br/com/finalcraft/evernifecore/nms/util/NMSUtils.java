eaea707f2bf26351893cd612cb62a3ec8e2d3e79
Pétrus Pradella <petrus_ant@hotmail.com>
2022-12-16 01:09:00 -0300

    Fix 'getLocalizedName' on NMSUtils for v1_16_R3

--- src/main/java/br/com/finalcraft/evernifecore/nms/util/NMSUtils.java
+++ src/main/java/br/com/finalcraft/evernifecore/nms/util/NMSUtils.java
@@ -21,21 +21,25 @@ public class NMSUtils {
 
 	private static void init() {
 		try {
-			switch (MCVersion.getCurrent()){
-				case v1_7_R4:
-					instance = new NMSUtils_v1_7_R4();
-					return;
-				case v1_12_R1:
-					instance = new NMSUtils_v1_12_R1();
-					return;
-				case v1_16_R3:
-					instance = new NMSUtils_v1_16_R3();
-					return;
+			if (MCVersion.isEqual(MCVersion.v1_7_10)){
+				instance = new NMSUtils_v1_7_R4();
+			}
+			if (MCVersion.isEqual(MCVersion.v1_12)){
+				instance = new NMSUtils_v1_12_R1();
+			}
+			if (MCVersion.isEqual(MCVersion.v1_16)){
+				instance = new NMSUtils_v1_16_R3();
+			}
+			if (instance != null){
+				EverNifeCore.info("Sucessfully loaded " + instance.getClass().getSimpleName() +"!");
+				return;
 			}
 		}catch (Throwable e){
 			EverNifeCore.warning("Failed to create NMSUtils instance for " + MCVersion.getCurrent().name() + " mc version!");
 			e.printStackTrace();
+			return;
 		}
+		EverNifeCore.warning("No NMSUtils instance found for " + MCVersion.getCurrent().name() + " mc version!");
 	}
 
 
