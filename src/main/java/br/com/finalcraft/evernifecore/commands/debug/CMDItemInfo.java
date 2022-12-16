8518a3d67bed44f596a32cf8bcfbd5daf3a4207c
Pétrus Pradella <petrus_ant@hotmail.com>
2022-12-16 03:18:03 -0300

    Add more info to /iteminfo

--- src/main/java/br/com/finalcraft/evernifecore/commands/debug/CMDItemInfo.java
+++ src/main/java/br/com/finalcraft/evernifecore/commands/debug/CMDItemInfo.java
@@ -6,7 +6,9 @@ import br.com.finalcraft.evernifecore.commands.finalcmd.annotations.FinalCMD;
 import br.com.finalcraft.evernifecore.fancytext.FancyText;
 import br.com.finalcraft.evernifecore.itemdatapart.ItemDataPart;
 import br.com.finalcraft.evernifecore.material.FCMaterialUtil;
+import br.com.finalcraft.evernifecore.nms.util.NMSUtils;
 import br.com.finalcraft.evernifecore.util.FCBukkitUtil;
+import br.com.finalcraft.evernifecore.util.FCItemUtils;
 import br.com.finalcraft.evernifecore.util.FCMessageUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -28,7 +30,19 @@ public class CMDItemInfo {
             return;
         }
 
-        String readLines = ItemDataPart.readItem(heldItem).stream().collect(Collectors.joining("\n"));
+        if (NMSUtils.get() != null){
+            FancyText.of("§d ● §eLocalizedName: §a" + FCItemUtils.getLocalizedName(heldItem))
+                    .setHoverText("Click to copy!")
+                    .setSuggestCommandAction(FCItemUtils.getLocalizedName(heldItem))
+                    .send(player);
+
+            FancyText.of("§d ● §eMCIdentifier: §a" + FCItemUtils.getMinecraftIdentifier(heldItem))
+                    .setHoverText("Click to copy!")
+                    .setSuggestCommandAction(FCItemUtils.getMinecraftIdentifier(heldItem))
+                    .send(player);
+        }
+
+        String readLines = "§a" + ItemDataPart.readItem(heldItem).stream().collect(Collectors.joining("\n"));
         FancyText.of(readLines)
                 .setHoverText(readLines)
                 .setSuggestCommandAction(readLines)
