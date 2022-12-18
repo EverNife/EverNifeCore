b2f616a08e26f74259328e6bb88a2873618dd1d9
Pétrus Pradella <petrus_ant@hotmail.com>
2022-12-18 06:08:01 -0300

    Validate items when create an "ItemInSlot"

--- src/main/java/br/com/finalcraft/evernifecore/inventory/data/ItemInSlot.java
+++ src/main/java/br/com/finalcraft/evernifecore/inventory/data/ItemInSlot.java
@@ -1,11 +1,9 @@
 package br.com.finalcraft.evernifecore.inventory.data;
 
+import br.com.finalcraft.evernifecore.itemstack.FCItemFactory;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.OptionalInt;
+import java.util.*;
 
 public class ItemInSlot {
 
@@ -29,7 +27,7 @@ public class ItemInSlot {
         int count = 0;
         for (ItemStack itemStack : itemStacks) {
             if (itemStack != null){
-                itemsInSlotList.add(new ItemInSlot(count, new ItemStack(itemStack)));
+                itemsInSlotList.add(new ItemInSlot(count, FCItemFactory.from(itemStack.clone()).build()));//Store a VALIDATED copy
             }
             count++;
         }
@@ -37,13 +35,7 @@ public class ItemInSlot {
     }
 
     public static List<ItemInSlot> fromStacks(ItemStack... itemStacks){
-        List<ItemInSlot> itemsInSlotList = new ArrayList<>();
-        for (int i = 0; i < itemStacks.length; i++) {
-            if (itemStacks[i] != null){
-                itemsInSlotList.add(new ItemInSlot(i, new ItemStack(itemStacks[i])));
-            }
-        }
-        return itemsInSlotList;
+        return fromStacks(Arrays.asList(itemStacks));
     }
 
     public static ItemStack[] toArray(Collection<ItemInSlot> itemInSlots){
@@ -51,7 +43,7 @@ public class ItemInSlot {
         if (!maxSize.isPresent()) return new ItemStack[0];
         ItemStack[] itemStacks = new ItemStack[maxSize.getAsInt() + 1];
         for (ItemInSlot itemInSlot : itemInSlots) {
-            itemStacks[itemInSlot.getSlot()] = itemInSlot.getItemStack();
+            itemStacks[itemInSlot.getSlot()] = itemInSlot.getItemStack();//Store a VALIDATED copy
         }
         return itemStacks;
     }
