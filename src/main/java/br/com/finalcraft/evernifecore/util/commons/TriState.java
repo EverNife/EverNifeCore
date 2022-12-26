ce71591ecfb3580fcdb7a8cc01b6887944a28756
Pétrus Pradella <petrus_ant@hotmail.com>
2022-12-26 19:55:21 -0300

    Add TriState class

---
+++ src/main/java/br/com/finalcraft/evernifecore/util/commons/TriState.java
@@ -0,0 +1,38 @@
+package br.com.finalcraft.evernifecore.util.commons;
+
+public enum TriState {
+    TRUE(true),
+    FALSE(false),
+    UNKNOWN(null);
+
+    private final Boolean value;
+
+    private TriState(Boolean value) {
+        this.value = value;
+    }
+
+    public static TriState of(Boolean value) {
+        return value == null ? UNKNOWN
+                : value ? TRUE : FALSE;
+    }
+
+    public boolean isTrue() {
+        return this == TRUE;
+    }
+
+    public boolean isFalse() {
+        return this == FALSE;
+    }
+
+    public boolean isUnknown() {
+        return this == UNKNOWN;
+    }
+
+    public boolean isKnown() {
+        return this != UNKNOWN;
+    }
+
+    public Boolean toBoolean() {
+        return value;
+    }
+}
