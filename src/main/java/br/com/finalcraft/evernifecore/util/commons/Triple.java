df82930508f4ce21b478d57f4cce2434a4809e70
Pétrus Pradella <petrus_ant@hotmail.com>
2022-12-27 19:49:46 -0300

    Refactor Tuple and Trile (again...)

--- src/main/java/br/com/finalcraft/evernifecore/util/commons/Triple.java
+++ src/main/java/br/com/finalcraft/evernifecore/util/commons/Triple.java
@@ -1,71 +1,24 @@
 package br.com.finalcraft.evernifecore.util.commons;
 
-public class Triple<ALFA, BETA, GAMA> {
+import lombok.Data;
+import lombok.experimental.Accessors;
 
-    public ALFA alfa;
-    public BETA beta;
-    public GAMA gama;
+@Data
+@Accessors(chain = true)
+public class Triple<LEFT, MIDDLE, RIGHT> {
 
-    private Triple(ALFA alfa, BETA beta, GAMA gama) {
-        this.alfa = alfa;
-        this.beta = beta;
-        this.gama = gama;
+    public LEFT left;
+    public MIDDLE middle;
+    public RIGHT right;
+
+    private Triple(LEFT left, MIDDLE middle, RIGHT right) {
+        this.left = left;
+        this.middle = middle;
+        this.right = right;
     }
 
     public static <ALFA,BETA,GAMA> Triple<ALFA,BETA,GAMA> of(ALFA alfa, BETA beta, GAMA gama) {
         return new Triple<>(alfa, beta, gama);
     }
 
-    public ALFA getAlfa() {
-        return alfa;
-    }
-
-    public void setAlfa(ALFA alfa) {
-        this.alfa = alfa;
-    }
-
-    public BETA getBeta() {
-        return beta;
-    }
-
-    public void setBeta(BETA beta) {
-        this.beta = beta;
-    }
-
-    public GAMA getGama() {
-        return gama;
-    }
-
-    public void setGama(GAMA gama) {
-        this.gama = gama;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Triple<?, ?, ?> triple = (Triple<?, ?, ?>) o;
-
-        if (alfa != null ? !alfa.equals(triple.alfa) : triple.alfa != null) return false;
-        if (beta != null ? !beta.equals(triple.beta) : triple.beta != null) return false;
-        return gama != null ? gama.equals(triple.gama) : triple.gama == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = alfa != null ? alfa.hashCode() : 0;
-        result = 31 * result + (beta != null ? beta.hashCode() : 0);
-        result = 31 * result + (gama != null ? gama.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Triple{" +
-                "alfa=" + alfa +
-                ", beta=" + beta +
-                ", gama=" + gama +
-                '}';
-    }
 }
--- src/main/java/br/com/finalcraft/evernifecore/util/commons/Tuple.java
+++ src/main/java/br/com/finalcraft/evernifecore/util/commons/Tuple.java
@@ -1,55 +1,22 @@
 package br.com.finalcraft.evernifecore.util.commons;
 
-import java.util.Objects;
+import lombok.Data;
+import lombok.experimental.Accessors;
 
+@Data
+@Accessors(chain = true)
 public class Tuple<ALFA, BETA> {
 
-    private ALFA alfa;
-    private BETA beta;
+    private ALFA left;
+    private BETA right;
 
-    private Tuple(ALFA alfa, BETA beta) {
-        this.alfa = alfa;
-        this.beta = beta;
-    }
-
-    public ALFA getAlfa() {
-        return alfa;
-    }
-
-    public void setAlfa(ALFA alfa) {
-        this.alfa = alfa;
-    }
-
-    public BETA getBeta() {
-        return beta;
-    }
-
-    public void setBeta(BETA beta) {
-        this.beta = beta;
+    private Tuple(ALFA left, BETA right) {
+        this.left = left;
+        this.right = right;
     }
 
     public static <ALFA, BETA> Tuple<ALFA, BETA> of(ALFA alfa, BETA beta){
         return new Tuple<>(alfa,beta);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Tuple<?, ?> tuple = (Tuple<?, ?>) o;
-        return Objects.equals(alfa, tuple.alfa) && Objects.equals(beta, tuple.beta);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(alfa, beta);
-    }
-
-    @Override
-    public String toString() {
-        return "Tuple{" +
-                "alfa=" + alfa +
-                ", beta=" + beta +
-                '}';
-    }
 }
