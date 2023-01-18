584e306db55cc0785a65d3a0704ea9a6e5eef5cf
Pétrus Pradella <petrus_ant@hotmail.com>
2023-01-18 01:55:31 -0300

    Add the default value for every single flag

--- modules/VersionedPlugins/src/main/java/br/com/finalcraft/evernifecore/protection/worldguard/WGFlags.java
+++ modules/VersionedPlugins/src/main/java/br/com/finalcraft/evernifecore/protection/worldguard/WGFlags.java
@@ -2,103 +2,107 @@ package br.com.finalcraft.evernifecore.protection.worldguard;
 
 import com.sk89q.worldguard.protection.flags.*;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.lang.reflect.Field;
 
 
 //This class is a mirror of https://github.com/EngineHub/WorldGuard/blob/master/worldguard-core/src/main/java/com/sk89q/worldguard/protection/flags/Flags.java
-// I had to do this because this class has change it's name
+// I had to do this because this class has changed its name
 // from "DefaultFlags" to "Flags" between WorldGuard 6.0 and 7.0
 public class WGFlags {
 
     // Overrides membership check
-    public static StateFlag PASSTHROUGH;
+    public static @DefaultState(state = false) StateFlag PASSTHROUGH;
 
     // This flag is unlike the others. It forces the checking of region membership
-    public static StateFlag BUILD;
+    public static @DefaultState(state = false) StateFlag BUILD;
 
     // These flags are used in tandem with the BUILD flag - if the player can
     // build, then the following flags do not need to be checked (although they
     // are still checked for DENY), so they are false by default
-    public static StateFlag BLOCK_BREAK;
-    public static StateFlag BLOCK_PLACE;
-    public static StateFlag USE;
-    public static StateFlag INTERACT;
-    public static StateFlag DAMAGE_ANIMALS;
-    public static StateFlag PVP;
-    public static StateFlag SLEEP;
-    public static StateFlag RESPAWN_ANCHORS;
-    public static StateFlag TNT;
-    public static StateFlag CHEST_ACCESS;
-    public static StateFlag PLACE_VEHICLE = (StateFlag) getFlagFromWorldGuard("vehicle-place");
-    public static StateFlag DESTROY_VEHICLE = (StateFlag) getFlagFromWorldGuard("vehicle-destroy");
-    public static StateFlag LIGHTER;
-    public static StateFlag RIDE;
-    public static StateFlag POTION_SPLASH;
-    public static StateFlag ITEM_FRAME_ROTATE = (StateFlag) getFlagFromWorldGuard("item-frame-rotation");
-    public static StateFlag TRAMPLE_BLOCKS = (StateFlag) getFlagFromWorldGuard("block-trampling");
-    public static StateFlag FIREWORK_DAMAGE;
-    public static StateFlag USE_ANVIL;
-    public static StateFlag USE_DRIPLEAF;
+    public static @DefaultState(state = false) StateFlag BLOCK_BREAK;
+    public static @DefaultState(state = false) StateFlag BLOCK_PLACE;
+    public static @DefaultState(state = false) StateFlag USE;
+    public static @DefaultState(state = false) StateFlag INTERACT;
+    public static @DefaultState(state = false) StateFlag DAMAGE_ANIMALS;
+    public static @DefaultState(state = false) StateFlag PVP;
+    public static @DefaultState(state = false) StateFlag SLEEP;
+    public static @DefaultState(state = false) StateFlag RESPAWN_ANCHORS;
+    public static @DefaultState(state = false) StateFlag TNT;
+    public static @DefaultState(state = false) StateFlag CHEST_ACCESS;
+    public static @DefaultState(state = false) StateFlag PLACE_VEHICLE = (StateFlag) getFlagFromWorldGuard("vehicle-place");
+    public static @DefaultState(state = false) StateFlag DESTROY_VEHICLE = (StateFlag) getFlagFromWorldGuard("vehicle-destroy");
+    public static @DefaultState(state = false) StateFlag LIGHTER;
+    public static @DefaultState(state = false) StateFlag RIDE;
+    public static @DefaultState(state = false) StateFlag POTION_SPLASH;
+    public static @DefaultState(state = false) StateFlag ITEM_FRAME_ROTATE = (StateFlag) getFlagFromWorldGuard("item-frame-rotation");
+    public static @DefaultState(state = false) StateFlag TRAMPLE_BLOCKS = (StateFlag) getFlagFromWorldGuard("block-trampling");
+    public static @DefaultState(state = false) StateFlag FIREWORK_DAMAGE;
+    public static @DefaultState(state = false) StateFlag USE_ANVIL;
+    public static @DefaultState(state = false) StateFlag USE_DRIPLEAF;
 
     // These flags are similar to the ones above (used in tandem with BUILD),
     // but their defaults are set to TRUE because it is more user friendly.
     // However, it is not possible to disable these flags by default in all
     // regions because setting DENY in __global__ would also override the
     // BUILD flag. In the future, StateFlags will need a DISALLOW state.
-    public static StateFlag ITEM_PICKUP; // Intentionally true
-    public static StateFlag ITEM_DROP; // Intentionally true
-    public static StateFlag EXP_DROPS; // Intentionally true
+    public static @DefaultState(state = true) StateFlag ITEM_PICKUP; // Intentionally true
+    public static @DefaultState(state = true) StateFlag ITEM_DROP; // Intentionally true
+    public static @DefaultState(state = true) StateFlag EXP_DROPS; // Intentionally true
 
     // These flags adjust behavior and are not checked in tandem with the
     // BUILD flag so they need to be TRUE for their defaults.
 
     // mob griefing related
-    public static StateFlag MOB_DAMAGE;
-    public static StateFlag CREEPER_EXPLOSION;
-    public static StateFlag ENDERDRAGON_BLOCK_DAMAGE;
-    public static StateFlag GHAST_FIREBALL;
-    public static StateFlag OTHER_EXPLOSION;
-    public static StateFlag WITHER_DAMAGE;
-    public static StateFlag ENDER_BUILD = (StateFlag) getFlagFromWorldGuard("enderman-grief");
-    public static StateFlag SNOWMAN_TRAILS;
-    public static StateFlag RAVAGER_RAVAGE;
-    public static StateFlag ENTITY_PAINTING_DESTROY;
-    public static StateFlag ENTITY_ITEM_FRAME_DESTROY;
+    public static @DefaultState(state = true) StateFlag MOB_DAMAGE;
+    public static @DefaultState(state = true) StateFlag CREEPER_EXPLOSION;
+    public static @DefaultState(state = true) StateFlag ENDERDRAGON_BLOCK_DAMAGE;
+    public static @DefaultState(state = true) StateFlag GHAST_FIREBALL;
+    public static @DefaultState(state = true) StateFlag OTHER_EXPLOSION;
+    public static @DefaultState(state = true) StateFlag WITHER_DAMAGE;
+    public static @DefaultState(state = true) StateFlag ENDER_BUILD = (StateFlag) getFlagFromWorldGuard("enderman-grief");
+    public static @DefaultState(state = true) StateFlag SNOWMAN_TRAILS;
+    public static @DefaultState(state = true) StateFlag RAVAGER_RAVAGE;
+    public static @DefaultState(state = true) StateFlag ENTITY_PAINTING_DESTROY;
+    public static @DefaultState(state = true) StateFlag ENTITY_ITEM_FRAME_DESTROY;
 
     // mob spawning related
-    public static StateFlag MOB_SPAWNING;
+    public static @DefaultState(state = true) StateFlag MOB_SPAWNING;
     public static SetFlag DENY_SPAWN;
 
     // block dynamics
-    public static StateFlag PISTONS;
-    public static StateFlag FIRE_SPREAD;
-    public static StateFlag LAVA_FIRE;
-    public static StateFlag LIGHTNING;
-    public static StateFlag SNOW_FALL;
-    public static StateFlag SNOW_MELT;
-    public static StateFlag ICE_FORM;
-    public static StateFlag ICE_MELT;
-    public static StateFlag FROSTED_ICE_MELT;
-    public static StateFlag FROSTED_ICE_FORM; // this belongs in the first category of "checked with build"
-    public static StateFlag MUSHROOMS = (StateFlag) getFlagFromWorldGuard("mushroom-growth");
-    public static StateFlag LEAF_DECAY;
-    public static StateFlag GRASS_SPREAD = (StateFlag) getFlagFromWorldGuard("grass-growth");
-    public static StateFlag MYCELIUM_SPREAD;
-    public static StateFlag VINE_GROWTH;
-    public static StateFlag ROCK_GROWTH;
-    public static StateFlag SCULK_GROWTH;
-    public static StateFlag CROP_GROWTH;
-    public static StateFlag SOIL_DRY;
-    public static StateFlag CORAL_FADE;
-    public static StateFlag WATER_FLOW;
-    public static StateFlag LAVA_FLOW;
+    public static @DefaultState(state = true) StateFlag PISTONS;
+    public static @DefaultState(state = true) StateFlag FIRE_SPREAD;
+    public static @DefaultState(state = true) StateFlag LAVA_FIRE;
+    public static @DefaultState(state = true) StateFlag LIGHTNING;
+    public static @DefaultState(state = true) StateFlag SNOW_FALL;
+    public static @DefaultState(state = true) StateFlag SNOW_MELT;
+    public static @DefaultState(state = true) StateFlag ICE_FORM;
+    public static @DefaultState(state = true) StateFlag ICE_MELT;
+    public static @DefaultState(state = true) StateFlag FROSTED_ICE_MELT;
+    public static @DefaultState(state = false) StateFlag FROSTED_ICE_FORM; // this belongs in the first category of "checked with build"
+    public static @DefaultState(state = true) StateFlag MUSHROOMS = (StateFlag) getFlagFromWorldGuard("mushroom-growth");
+    public static @DefaultState(state = true) StateFlag LEAF_DECAY;
+    public static @DefaultState(state = true) StateFlag GRASS_SPREAD = (StateFlag) getFlagFromWorldGuard("grass-growth");
+    public static @DefaultState(state = true) StateFlag MYCELIUM_SPREAD;
+    public static @DefaultState(state = true) StateFlag VINE_GROWTH;
+    public static @DefaultState(state = true) StateFlag ROCK_GROWTH;
+    public static @DefaultState(state = true) StateFlag SCULK_GROWTH;
+    public static @DefaultState(state = true) StateFlag CROP_GROWTH;
+    public static @DefaultState(state = true) StateFlag SOIL_DRY;
+    public static @DefaultState(state = true) StateFlag CORAL_FADE;
+    public static @DefaultState(state = true) StateFlag WATER_FLOW;
+    public static @DefaultState(state = true) StateFlag LAVA_FLOW;
 
     public static Flag WEATHER_LOCK; //<WeatherType> Its EnumFlag on 6.1 and RegistryFlag on 7.1
     public static StringFlag TIME_LOCK;
 
     // chat related flags
-    public static StateFlag SEND_CHAT;
-    public static StateFlag RECEIVE_CHAT;
+    public static @DefaultState(state = true) StateFlag SEND_CHAT;
+    public static @DefaultState(state = true) StateFlag RECEIVE_CHAT;
     public static SetFlag<String> BLOCKED_CMDS;
     public static SetFlag<String> ALLOWED_CMDS;
 
@@ -108,19 +112,19 @@ public class WGFlags {
 
 
     // idk?
-    public static StateFlag INVINCIBILITY;
-    public static StateFlag FALL_DAMAGE;
-    public static StateFlag HEALTH_REGEN;
-    public static StateFlag HUNGER_DRAIN;
+    public static @DefaultState(state = false) StateFlag INVINCIBILITY;
+    public static @DefaultState(state = true) StateFlag FALL_DAMAGE;
+    public static @DefaultState(state = true) StateFlag HEALTH_REGEN;
+    public static @DefaultState(state = true) StateFlag HUNGER_DRAIN;
 
     // session and movement based flags
-    public static StateFlag ENTRY;
-    public static StateFlag EXIT;
+    public static @DefaultState(state = true) StateFlag ENTRY;
+    public static @DefaultState(state = true) StateFlag EXIT;
     public static BooleanFlag EXIT_OVERRIDE;
-    public static StateFlag EXIT_VIA_TELEPORT;
+    public static @DefaultState(state = true) StateFlag EXIT_VIA_TELEPORT;
 
-    public static StateFlag ENDERPEARL;
-    public static StateFlag CHORUS_TELEPORT;
+    public static @DefaultState(state = true) StateFlag ENDERPEARL;
+    public static @DefaultState(state = true) StateFlag CHORUS_TELEPORT;
 
     /**
      * @deprecated The type of this flag will change from a StringFlag to a ComponentFlag to support JSON text
@@ -200,5 +204,17 @@ public class WGFlags {
                 //e.printStackTrace();
             }
         }
+    }
+
+    /**
+     * This is a Descriminator for the Flags, to tell the default value of the Flag
+     *
+     */
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.FIELD})
+    public static @interface DefaultState {
+
+        boolean state();
+
     }
 }
