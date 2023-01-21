fa40f3ad34c9601155e87a230b4480177ed2bcdc
Pétrus Pradella <petrus_ant@hotmail.com>
2023-01-21 17:19:39 -0300

    Add several utlity functions to IProvider

--- src/main/java/br/com/finalcraft/evernifecore/placeholder/base/IProvider.java
+++ src/main/java/br/com/finalcraft/evernifecore/placeholder/base/IProvider.java
@@ -5,9 +5,48 @@ import java.util.function.Function;
 
 public interface IProvider<O extends Object> {
 
+    public default IProvider<O> addParser(String name, Object obj){
+        this.addParser(name, o -> obj);
+        return this;
+    }
+
+    public default IProvider<O> addParser(String[] name, Object obj){
+        for (String alias : name) {
+            this.addParser(alias, o -> obj);
+        }
+        return this;
+    }
+
     public IProvider<O> addParser(String name, Function<O, Object> parser);
 
+    public default IProvider<O> addParser(String[] name, Function<O, Object> parser){
+        for (String alias : name) {
+            this.addParser(alias, parser);
+        }
+        return this;
+    }
+
+    public default IProvider<O> addParser(String name, String description, Object obj){
+        this.addParser(name, description, o -> obj);
+        return this;
+    }
+
+
+    public default IProvider<O> addParser(String[] name, String description, Object obj){
+        for (String alias : name) {
+            this.addParser(alias, description, o -> obj);
+        }
+        return this;
+    }
+
     public IProvider<O> addParser(String name, String description, Function<O, Object> parser);
+
+    public default IProvider<O> addParser(String[] name, String description, Function<O, Object> parser){
+        for (String alias : name) {
+            this.addParser(alias, description, parser);
+        }
+        return this;
+    }
 
     public IProvider<O> setDefaultParser(BiFunction<O, String, Object> defaultParser);
 
