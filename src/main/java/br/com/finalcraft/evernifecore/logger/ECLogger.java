76bb492929fbde48e20f902c6bd023b7e3a9aa22
Pétrus Pradella <petrus_ant@hotmail.com>
2023-02-19 05:20:23 -0300

    Properly add moduled logging for ECLogger

--- src/main/java/br/com/finalcraft/evernifecore/logger/ECLogger.java
+++ src/main/java/br/com/finalcraft/evernifecore/logger/ECLogger.java
@@ -46,20 +46,6 @@ public class ECLogger<DL extends IDebugModule> {
         }
     }
 
-    public void debugModule(DL debugModule, String message, Object... params) {
-        if (getEcPluginData().isDebugEnabled(debugModule)){
-            String formatted = "[Debug (" + debugModule.getName()  + ") ] " + (params.length == 0 ? message : String.format(message, params));
-            plugin.getLogger().info(formatted);
-        }
-    }
-
-    public void debugModule(DL debugModule, Supplier<String> supplier) {
-        if (getEcPluginData().isDebugEnabled(debugModule)){
-            String formatted = "[Debug (" + debugModule.getName()  + ") ] " + supplier.get();
-            plugin.getLogger().info(formatted);
-        }
-    }
-
     public void info(String message, Object... params) {
         String formatted = params.length == 0 ? message : String.format(message, params);
         plugin.getLogger().info(formatted);
@@ -85,6 +71,56 @@ public class ECLogger<DL extends IDebugModule> {
 
     public void severe(Supplier<String> supplier) {
         plugin.getLogger().severe(supplier);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    //  Modulirized Logging
+    // -----------------------------------------------------------------------------------------------------------------
+
+    public void logModule(DL debugModule, Level level, String msg) {
+        this.log(level, "[" + debugModule.getName() + "] " + msg);
+    }
+
+    public void logModule(DL debugModule, Level level, Supplier<String> supplier) {
+        this.log(level, () -> "[" + debugModule.getName() + "] " + supplier.get());
+    }
+
+    public void debugModule(DL debugModule, String message, Object... params) {
+        if (getEcPluginData().isDebugEnabled(debugModule)){
+            String formatted = "[Debug (" + debugModule.getName()  + ") ] " + (params.length == 0 ? message : String.format(message, params));
+            plugin.getLogger().info(formatted);
+        }
+    }
+
+    public void debugModule(DL debugModule, Supplier<String> supplier) {
+        if (getEcPluginData().isDebugEnabled(debugModule)){
+            String formatted = "[Debug (" + debugModule.getName()  + ") ] " + supplier.get();
+            plugin.getLogger().info(formatted);
+        }
+    }
+
+    public void infoModule(DL debugModule, String message, Object... params) {
+        this.info("[" + debugModule.getName() + "] " + message, params);
+    }
+
+    public void infoModule(DL debugModule, Supplier<String> supplier) {
+        this.info(() -> "[" + debugModule.getName() + "] " + supplier.get());
+    }
+
+    public void warningModule(DL debugModule, String message, Object... params) {
+        this.warning("[" + debugModule.getName() + "] " + message, params);
+    }
+
+    public void warningModule(DL debugModule, Supplier<String> supplier) {
+        this.warning(() -> "[" + debugModule.getName() + "] " + supplier.get());
+    }
+
+    public void severeModule(DL debugModule, String message, Object... params) {
+        this.severe("[" + debugModule.getName() + "] " + message, params);
+    }
+
+    public void severeModule(DL debugModule, Supplier<String> supplier) {
+        this.severe(() -> "[" + debugModule.getName() + "] " + supplier.get());
     }
 
 }
