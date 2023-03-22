9a9ef4b2c63aec3ebbd89ba5ab51d2ccb106b8de
Pétrus Pradella <petrus_ant@hotmail.com>
2023-03-21 21:18:16 -0300

    Add 'runOnlyOnce' arg to FCCommandUtil::dynamicCommand

--- src/main/java/br/com/finalcraft/evernifecore/util/FCCommandUtil.java
+++ src/main/java/br/com/finalcraft/evernifecore/util/FCCommandUtil.java
@@ -27,9 +27,23 @@ public class FCCommandUtil {
      * @return The Command link to this runnable, like "/%label% UUID"
      */
     public static String dynamicCommand(Runnable runnable, long maxTimeInSeconds){
+        return dynamicCommand(runnable, maxTimeInSeconds, false);
+    }
+
+    /**
+     * Creates a Command String that will execute this runnable when the command
+     * is executed by the player
+     *
+     * @param runnable The runnable for this command
+     * @param maxTimeInSeconds The max amount of time this command can be run
+     *
+     * @return The Command link to this runnable, like "/%label% UUID"
+     */
+    public static String dynamicCommand(Runnable runnable, long maxTimeInSeconds, boolean runOnlyOnce){
         return DynamicCommand.builder()
                 .setAction(context -> runnable.run())
                 .setCooldown(maxTimeInSeconds)
+                .setRunOnlyOnce(runOnlyOnce)
                 .createDynamicCommand()
                 .scheduleAndReturnCommandString();
     }
