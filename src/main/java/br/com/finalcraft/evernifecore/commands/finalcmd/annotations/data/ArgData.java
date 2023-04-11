62bce5fb070586e48db123c0dd3764cca540329b
Pétrus Pradella <petrus_ant@hotmail.com>
2023-04-11 06:15:52 -0300

    Add locales to argdata and allow it to be regex-replaced as well

--- src/main/java/br/com/finalcraft/evernifecore/commands/finalcmd/annotations/data/ArgData.java
+++ src/main/java/br/com/finalcraft/evernifecore/commands/finalcmd/annotations/data/ArgData.java
@@ -1,11 +1,16 @@
 package br.com.finalcraft.evernifecore.commands.finalcmd.annotations.data;
 
 import br.com.finalcraft.evernifecore.commands.finalcmd.annotations.Arg;
+import br.com.finalcraft.evernifecore.commands.finalcmd.annotations.FlagArg;
 import br.com.finalcraft.evernifecore.commands.finalcmd.argument.ArgParser;
+import br.com.finalcraft.evernifecore.locale.data.FCLocaleData;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -16,11 +21,27 @@ public class ArgData {
     private String name;
     private String context;
     private Class<? extends ArgParser> parser;
+    private FCLocaleData[] locales = new FCLocaleData[0];
+    private boolean flag = false;
 
     public ArgData(Arg arg) {
         this.name = arg.name();
         this.context = arg.context();
         this.parser = arg.parser();
+        this.locales = Arrays.stream(arg.locales())
+                .map(FCLocaleData::new)
+                .collect(Collectors.toList())
+                .toArray(new FCLocaleData[0]);
+    }
+
+    public ArgData(FlagArg arg) {
+        this.name = arg.name();
+        this.context = arg.context();
+        this.parser = arg.parser();
+        this.locales = Arrays.stream(arg.locales())
+                .map(FCLocaleData::new)
+                .collect(Collectors.toList())
+                .toArray(new FCLocaleData[0]);
     }
 
     public ArgData() {
@@ -32,6 +53,9 @@ public class ArgData {
     public ArgData replace(String placeholder, String value){
         this.name = this.name.replace(placeholder, value);
         this.context = this.context.replace(placeholder, value);
+        for (FCLocaleData locale : this.locales) {
+            locale.replace(placeholder, value);
+        }
         return this;
     }
 }
