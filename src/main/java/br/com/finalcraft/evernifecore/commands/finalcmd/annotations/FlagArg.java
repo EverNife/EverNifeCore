f8bec9ccaf256eef15a37f1b8f7f8dd98b26c44f
Pétrus Pradella <petrus_ant@hotmail.com>
2023-04-11 06:03:38 -0300

    Separate @Arg from @FlagArg

--- src/main/java/br/com/finalcraft/evernifecore/commands/finalcmd/annotations/FlagArg.java
+++ src/main/java/br/com/finalcraft/evernifecore/commands/finalcmd/annotations/FlagArg.java
@@ -10,7 +10,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.PARAMETER})
-public @interface Arg {
+public @interface FlagArg {
 
     String name();
 
