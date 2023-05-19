229e07fdd783880152b86f0533ae26f381aa02fa
Pétrus Pradella <petrus_ant@hotmail.com>
2023-05-19 02:00:57 -0300

    Fix test and add javadoc

--- src/main/java/br/com/finalcraft/evernifecore/util/FCCollectionsUtil.java
+++ src/main/java/br/com/finalcraft/evernifecore/util/FCCollectionsUtil.java
@@ -11,6 +11,23 @@ public class FCCollectionsUtil {
         return list;
     }
 
+    /**
+     * Splits a list of elements into a specified number of sublists, evenly distributing the elements.
+     *
+     * @param elements the list of elements to be split
+     * @param parts    the number of sublists to split the list into
+     * @return a list of sublists, each containing an equal distribution of elements
+     *
+     * @example:
+     *    // Input: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"], 4
+     *    // Output: [["a", "b", "c"], ["d", "e"], ["f", "g", "h"], ["i", "j"]]
+     *
+     *    // Input: ["a", "b"], 7
+     *    // Output: [["a"], [], [], ["b"], [], [], []]
+     *
+     *    // Input: ["a", "b"], 8
+     *    // Output: [["a"], [], [], [], ["b"], [], [], []]
+     */
     public static <T> List<List<T>> partitionEvenly(List<T> elements, int parts){
         List<List<T>> result = new ArrayList<>(parts);
 
--- src/test/java/br/com/finalcraft/evernifecore/test/util/FCCollectionUtilTest.java
+++ src/test/java/br/com/finalcraft/evernifecore/test/util/FCCollectionUtilTest.java
@@ -10,7 +10,7 @@ public class FCCollectionUtilTest {
 
     @Test
     public void testSpliter() {
-        List<String> parts = Arrays.asList("a","b","c","d","e","f","x","h","i","j","k","l","m","n"); //14 elements
+        List<String> parts = Arrays.asList("a","b","c","d","e","f","g","h","i","j","k","l","m","n"); //14 elements
 
         List<List<String>> lists = FCCollectionsUtil.partitionEvenly(parts, 6);
 
@@ -20,15 +20,23 @@ public class FCCollectionUtilTest {
             formated = formated + line + "\n";
         }
 
-        System.out.println(formated);
-        assert formated.contains(
-                "List 0: [a, b, c]\n" +
-                        "List 1: [d, e]\n" +
-                        "List 2: [f, g]\n" +
-                        "List 3: [h, i, j]\n" +
-                        "List 4: [k, l]\n" +
-                        "List 5: [m, n]"
+        System.out.println(formated + "\n\n");
+        assert formated.contains("List 0: [a, b, c]\n" +
+                "List 1: [d, e]\n" +
+                "List 2: [f, g]\n" +
+                "List 3: [h, i, j]\n" +
+                "List 4: [k, l]\n" +
+                "List 5: [m, n]"
         );
+
+        parts = Arrays.asList("a","b"); //2 elements
+        lists = FCCollectionsUtil.partitionEvenly(parts, 7);
+        formated = "";
+        for (int i = 0; i < lists.size(); i++) {
+            String line = "List " + i + ": " + Arrays.asList(lists.get(i).toArray());
+            formated = formated + line + "\n";
+        }
+        System.out.println(formated);
     }
 
 }
