1f671f59058636f9ba622df3e4eb4bb077fc69f7
Pétrus Pradella <petrus_ant@hotmail.com>
2024-04-09 12:25:38 -0300

    Add more detailed debug info for ItemDataPartNBT::transform

--- src/main/java/br/com/finalcraft/evernifecore/itemdatapart/datapart/ItemDataPartNBT.java
+++ src/main/java/br/com/finalcraft/evernifecore/itemdatapart/datapart/ItemDataPartNBT.java
@@ -2,6 +2,7 @@ package br.com.finalcraft.evernifecore.itemdatapart.datapart;
 
 import br.com.finalcraft.evernifecore.itemdatapart.ItemDataPart;
 import br.com.finalcraft.evernifecore.itemstack.FCItemFactory;
+import br.com.finalcraft.evernifecore.util.FCItemUtils;
 import br.com.finalcraft.evernifecore.util.FCNBTUtil;
 import br.com.finalcraft.evernifecore.version.MCVersion;
 import de.tr7zw.changeme.nbtapi.NBTCompound;
@@ -13,9 +14,30 @@ public class ItemDataPartNBT extends ItemDataPart {
 
     @Override
     public ItemStack transform(ItemStack item, String used_name, String argument) {
-        return FCItemFactory.from(item)
-                .setNbt(FCNBTUtil.getFrom(argument.trim()))
-                .build();
+        try {
+            return FCItemFactory.from(item)
+                    .setNbt(FCNBTUtil.getFrom(argument.trim()))
+                    .build();
+        }catch (Exception e){
+            e.printStackTrace();
+
+            String itemIdentifier;
+
+            try {
+                itemIdentifier = FCItemUtils.getMinecraftIdentifier(item);
+            } catch (Exception e2) {
+                e2.printStackTrace();
+                itemIdentifier = "[ITEM_IS_CORRUPTED]";
+            }
+
+            throw new RuntimeException(String.format(
+                    "[EverNifeCore] Failed to transform NBT data for the item: %s" +
+                            "\n  - itemIdentifier: %s" +
+                            "\n  - itemNBT: %s" +
+                            "\n  - used_name: %s",
+                    itemIdentifier, argument, used_name
+            ));
+        }
     }
 
     @Override
