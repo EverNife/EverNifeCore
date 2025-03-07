4fc44663c1be0949cfeb3354ae9497f8288708e3
Pétrus Pradella <petrus_ant@hotmail.com>
2025-03-07 18:11:48 -0300

    Optimize vanilla material check by introducing isForgeBlock method reflection

--- src/main/java/br/com/finalcraft/evernifecore/material/interpreters/MaterialVanillaInterpreter.java
+++ src/main/java/br/com/finalcraft/evernifecore/material/interpreters/MaterialVanillaInterpreter.java
@@ -1,6 +1,8 @@
 package br.com.finalcraft.evernifecore.material.interpreters;
 
+import br.com.finalcraft.evernifecore.reflection.MethodInvoker;
 import br.com.finalcraft.evernifecore.util.FCItemUtils;
+import br.com.finalcraft.evernifecore.util.FCReflectionUtil;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
@@ -10,9 +12,16 @@ import java.util.Map;
 public class MaterialVanillaInterpreter {
 
     private Map<Material, Boolean> isVanilla = new HashMap();
+    private MethodInvoker<Boolean> isForgeBlock = null;
 
     public MaterialVanillaInterpreter() {
         this.isVanilla.put(Material.AIR, true);
+
+        try {
+            isForgeBlock = FCReflectionUtil.getMethod(Material.class, "isForgeBlock");
+        }catch (Exception ignored) {
+            //Do Nothing
+        }
     }
 
     public boolean isVanilla(Material material) {
@@ -21,16 +30,35 @@ public class MaterialVanillaInterpreter {
             return vanilla;
         }
 
-        try {
-            ItemStack itemStack = new ItemStack(material);
-            String identifier = FCItemUtils.getMinecraftIdentifier(itemStack, false);
-            if (identifier.startsWith("minecraft:")){
-                vanilla = true;
-            }else {
+        if (isForgeBlock != null){
+            //Do a first scan based on isBlock status as it's a lot faster
+            try {
+                boolean materialIsFromForgeBlock = isForgeBlock.invoke(material);
+
+                if (materialIsFromForgeBlock){
+                    vanilla = false;
+                }else if (material.isBlock()){
+                    vanilla = true;
+                }
+
+            }catch (Exception e){
+
+            }
+        }
+
+        if (vanilla == null){
+            //As it's not a block, lets get the ItemName and check the prefix
+            try {
+                ItemStack itemStack = new ItemStack(material);
+                String identifier = FCItemUtils.getMinecraftIdentifier(itemStack, false);
+                if (identifier.startsWith("minecraft:")){
+                    vanilla = true;
+                }else {
+                    vanilla = false;
+                }
+            }catch (Exception ignored){
                 vanilla = false;
             }
-        }catch (Exception ignored){
-            vanilla = false;
         }
 
         isVanilla.put(material, vanilla);
