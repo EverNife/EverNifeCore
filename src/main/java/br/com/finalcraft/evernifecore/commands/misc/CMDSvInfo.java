effac77391c7ececd1c6755da5c29924b3abc22c
Pétrus Pradella <petrus_ant@hotmail.com>
2025-10-04 18:26:00 -0300

    Add JavaVersion at /svinfo

--- src/main/java/br/com/finalcraft/evernifecore/commands/misc/CMDSvInfo.java
+++ src/main/java/br/com/finalcraft/evernifecore/commands/misc/CMDSvInfo.java
@@ -2,6 +2,7 @@ package br.com.finalcraft.evernifecore.commands.misc;
 
 import br.com.finalcraft.evernifecore.EverNifeCore;
 import br.com.finalcraft.evernifecore.commands.finalcmd.annotations.FinalCMD;
+import br.com.finalcraft.evernifecore.version.FCJavaVersion;
 import br.com.finalcraft.evernifecore.version.MCVersion;
 import br.com.finalcraft.evernifecore.version.ServerType;
 import org.bukkit.Bukkit;
@@ -17,6 +18,7 @@ public class CMDSvInfo {
         sender.sendMessage("");
         sender.sendMessage(" §a - McVersion: §e" + MCVersion.getCurrent().name());
         sender.sendMessage(" §a - SvPlatform: §e" + Bukkit.getName());
+        sender.sendMessage(" §a - JavaVersion: §e" + FCJavaVersion.getCurrent().getName());
         if (ServerType.isEverNifePersonalServer()){
             sender.sendMessage(" §d - ServerName: §a" + ServerType.getCurrent().getName());
         }
