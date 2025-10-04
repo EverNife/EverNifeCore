9ba2cb6c4618ada01bc002612f66c9b007f73908
Pétrus Pradella <petrus_ant@hotmail.com>
2025-10-04 18:25:45 -0300

    Simplify FCThreadUtil

--- src/main/java/br/com/finalcraft/evernifecore/util/FCThreadUtil.java
+++ src/main/java/br/com/finalcraft/evernifecore/util/FCThreadUtil.java
@@ -4,20 +4,12 @@ import br.com.finalcraft.evernifecore.util.commons.MinMax;
 
 public class FCThreadUtil {
 
-    public static int getSystemThreadCount() {
-        return Thread.getAllStackTraces().keySet().size();
-    }
-
-    public static int getSystemCoreCount() {
-        return Runtime.getRuntime().availableProcessors();
-    }
-
     public static MinMax<Integer> getMinMaxThreadCountBoundedToSystemCoreCount(int max) {
         return getMinMaxThreadCountBoundedToSystemCoreCount(1, max);
     }
 
     public static MinMax<Integer> getMinMaxThreadCountBoundedToSystemCoreCount(int min, int max) {
-        int coreCount = getSystemCoreCount();
+        int coreCount = Runtime.getRuntime().availableProcessors();
         min = Math.max(min, 1); // Ensure min is at least 1
         max = Math.max(max, min); // Ensure max is at least min
         max = Math.min(max, coreCount); // Ensure max does not exceed core count
