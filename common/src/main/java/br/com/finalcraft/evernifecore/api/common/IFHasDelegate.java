7a30a773b19b90fb79b80595bceda0007b236e8f
Pétrus Pradella <petrus_ant@hotmail.com>
2026-03-15 11:22:51 -0300

    Start the creation of a common submodule

--- common/src/main/java/br/com/finalcraft/evernifecore/api/common/IFHasDelegate.java
+++ common/src/main/java/br/com/finalcraft/evernifecore/api/common/IFHasDelegate.java
@@ -1,7 +1,5 @@
 package br.com.finalcraft.evernifecore.api.common;
 
-import org.apache.commons.lang3.Validate;
-
 public interface IFHasDelegate {
 
     public Object getDelegate();
@@ -10,7 +8,6 @@ public interface IFHasDelegate {
         if (!delegateClass.isAssignableFrom(this.getDelegate().getClass())){
             throw new IllegalArgumentException("Invalid Delegate Conversion: #" + this.getClass().getSimpleName() + " | Requested conversion target class [" + delegateClass.getName() + "] is not assignable from the exising real delegate " + this.getDelegate().getClass().getName());
         }
-        Validate.isTrue(delegateClass.isAssignableFrom(this.getDelegate().getClass()), "");
         return (DELEGATE) getDelegate();
     }
 
