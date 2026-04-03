9b352671a584c8465072ba0ba5ff9756f7dc8b86
Pétrus Pradella <petrus_ant@hotmail.com>
2026-04-03 19:29:27 -0300

    [TheRework] Move libby to a separated sup-project

--- libby/src/main/java/br/com/finalcraft/evernifecore/dependencies/DependencyManager.java
+++ libby/src/main/java/br/com/finalcraft/evernifecore/dependencies/DependencyManager.java
@@ -1,8 +1,5 @@
 package br.com.finalcraft.evernifecore.dependencies;
 
-import br.com.finalcraft.evernifecore.util.FCExecutorsUtil;
-import br.com.finalcraft.evernifecore.util.FCJavaPluginUtil;
-import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import net.byteflux.libby.Library;
 import net.byteflux.libby.LibraryManager;
 import net.byteflux.libby.classloader.URLClassLoaderHelper;
@@ -12,7 +9,6 @@ import java.io.File;
 import java.net.URLClassLoader;
 import java.nio.file.Path;
 import java.util.Collection;
-import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.logging.Logger;
@@ -21,19 +17,17 @@ public class DependencyManager extends LibraryManager {
 
     private final URLClassLoaderHelper classLoader;
 
-    public DependencyManager() {
-        super(new JDKLogAdapter(Logger.getLogger("DefaultDependencyManager")), new File("mods/EverNife_EverNifeHyCore/").toPath());
-        classLoader = new URLClassLoaderHelper((URLClassLoader) DependencyManager.class.getClassLoader(), this);
+    public DependencyManager(String pluginName, File pluginRootFolder, String libsFolderName) {
+        this(pluginName, pluginRootFolder, libsFolderName, DependencyManager.class.getClassLoader());
     }
 
-    public DependencyManager(JavaPlugin plugin) {
-        super(new JDKLogAdapter(Logger.getLogger(Objects.requireNonNull(plugin, "plugin").getName())), FCJavaPluginUtil.getDataFolder(plugin).toPath());
-        classLoader = new URLClassLoaderHelper((URLClassLoader) plugin.getClass().getClassLoader(), this);
-    }
-
-    public DependencyManager(JavaPlugin plugin, Path dataDirectory, String folderName) {
-        super(new JDKLogAdapter(Logger.getLogger(Objects.requireNonNull(plugin, "plugin").getName())), dataDirectory, folderName);
-        classLoader = new URLClassLoaderHelper((URLClassLoader) plugin.getClass().getClassLoader(), this);
+    public DependencyManager(String pluginName, File pluginRootFolder, String libsFolderName, ClassLoader classLoader) {
+        super(
+                new JDKLogAdapter(Logger.getLogger("DependencyManager_" + pluginName)),
+                pluginRootFolder.toPath(),
+                libsFolderName
+        );
+        this.classLoader = new URLClassLoaderHelper((URLClassLoader) classLoader, this);
     }
 
     /**
