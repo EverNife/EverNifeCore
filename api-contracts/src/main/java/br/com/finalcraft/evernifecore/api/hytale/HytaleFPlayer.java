6cb6712656cb671a5f697198ea8e469b7605078d
Pétrus Pradella <petrus_ant@hotmail.com>
2026-04-03 19:30:06 -0300

    [TheRework] Create an api-contracts submodule to make my hacky life easier

---
+++ api-contracts/src/main/java/br/com/finalcraft/evernifecore/api/hytale/HytaleFPlayer.java
@@ -0,0 +1,5 @@
+package br.com.finalcraft.evernifecore.api.hytale;
+
+public class HytaleFPlayer {
+
+}
---
+++ api-contracts/src/main/java/br/com/finalcraft/evernifecore/hytale/argumento/HytaleArgumento.java
@@ -0,0 +1,8 @@
+package br.com.finalcraft.evernifecore.hytale.argumento;
+
+public class HytaleArgumento {
+
+    public HytaleArgumento(String argumento) {
+
+    }
+}
