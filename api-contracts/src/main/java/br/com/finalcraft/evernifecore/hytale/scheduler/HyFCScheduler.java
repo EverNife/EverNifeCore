65cda5162a9c8c3d577d3ef31989ce78b557973c
Pétrus Pradella <petrus_ant@hotmail.com>
2026-04-04 05:30:03 -0300

    [TheRework#hytale] Add more api-contracts

---
+++ api-contracts/src/main/java/br/com/finalcraft/evernifecore/hytale/scheduler/HyFCScheduler.java
@@ -0,0 +1,7 @@
+package br.com.finalcraft.evernifecore.hytale.scheduler;
+
+public class HyFCScheduler {
+
+    public static HyFCScheduler INSTANCE;
+
+}
---
+++ api-contracts/src/main/java/br/com/finalcraft/evernifecore/hytale/scheduler/McFCScheduler.java
@@ -0,0 +1,7 @@
+package br.com.finalcraft.evernifecore.hytale.scheduler;
+
+public class McFCScheduler {
+
+    public static McFCScheduler INSTANCE;
+
+}
