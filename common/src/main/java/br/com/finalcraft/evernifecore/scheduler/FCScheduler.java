c3d4047797ed73d93fae665becce39874cc7b073
Pétrus Pradella <petrus_ant@hotmail.com>
2026-04-04 05:32:04 -0300

    [TheRework#hytale] Separate FCScheduler logic from common and hytale specific ones with instances

---
+++ common/src/main/java/br/com/finalcraft/evernifecore/scheduler/FCScheduler.java
@@ -0,0 +1,61 @@
+package br.com.finalcraft.evernifecore.scheduler;
+
+import br.com.finalcraft.evernifecore.hytale.scheduler.HyFCScheduler;
+import br.com.finalcraft.evernifecore.hytale.scheduler.McFCScheduler;
+
+import java.util.concurrent.*;
+
+public class FCScheduler {
+
+    /**
+     * A Scheduler that uses Virtual Threads to execute the tasks.
+     * This allows for non-blocking scheduling of tasks that may perform blocking operations.
+     * Ideal for workloads where tasks may call Thread.sleep(), wait on I/O, or perform blocking operations.
+     * <p>
+     * This class requires Java 21 or higher to utilize virtual threads.
+     * On lower Java versions, it falls back to a fixed thread pool of System Core Count.
+     *
+     * If you are on a Java version lower than 21, this will use a normal Thread Pool instead of Virtual Threads,
+     * bounded to the number of CPU cores available on the system.
+     *
+     * See: {@link VirtualThreadedScheduledExecutor}
+     */
+    private static final VirtualThreadedScheduledExecutor scheduler = new VirtualThreadedScheduledExecutor("fcscheduler");
+
+    public static VirtualThreadedScheduledExecutor getScheduler() {
+        return scheduler;
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    //  Actions to be Executed on a Parallel Thread
+    // -----------------------------------------------------------------------------------------------------------------
+
+    public static void runAsync(Runnable runnable){
+        scheduler.execute(() -> {
+            try {
+                runnable.run();
+            }catch (Throwable throwable){
+                throwable.printStackTrace();
+            }
+        });
+    }
+
+    public static void scheduleAsync(Runnable runnable, long delayMillis){
+        scheduler.schedule(() -> {
+            try {
+                runnable.run();
+            }catch (Throwable throwable){
+                throwable.printStackTrace();
+            }
+        }, delayMillis, TimeUnit.MILLISECONDS);
+    }
+
+    public static HyFCScheduler getHytaleScheduler() {
+        return HyFCScheduler.INSTANCE;
+    }
+
+    public static McFCScheduler getMinecraftScheduler() {
+        return McFCScheduler.INSTANCE;
+    }
+
+}
--- hytale/src/main/java/br/com/finalcraft/evernifecore/hytale/scheduler/HyFCScheduler.java
+++ hytale/src/main/java/br/com/finalcraft/evernifecore/hytale/scheduler/HyFCScheduler.java
@@ -1,69 +1,39 @@
-package br.com.finalcraft.evernifecore.scheduler;
+package br.com.finalcraft.evernifecore.hytale.scheduler;
 
+import br.com.finalcraft.evernifecore.scheduler.FCScheduler;
 import com.hypixel.hytale.server.core.universe.world.World;
 
 import java.util.concurrent.*;
 
-public class FCScheduler {
+public class HyFCScheduler {
 
-    /**
-     * A Scheduler that uses Virtual Threads to execute the tasks.
-     * This allows for non-blocking scheduling of tasks that may perform blocking operations.
-     * Ideal for workloads where tasks may call Thread.sleep(), wait on I/O, or perform blocking operations.
-     * <p>
-     * This class requires Java 21 or higher to utilize virtual threads.
-     * On lower Java versions, it falls back to a fixed thread pool of System Core Count.
-     *
-     * If you are on a Java version lower than 21, this will use a normal Thread Pool instead of Virtual Threads,
-     * bounded to the number of CPU cores available on the system.
-     *
-     * See: {@link VirtualThreadedScheduledExecutor}
-     */
-    private static final VirtualThreadedScheduledExecutor scheduler = new VirtualThreadedScheduledExecutor("fcscheduler");
+    public static HyFCScheduler INSTANCE = new HyFCScheduler();
 
-    public static VirtualThreadedScheduledExecutor getScheduler() {
-        return scheduler;
+    private final SynchronizedAction synchronizedAction = new SynchronizedAction();
+
+    public HyFCScheduler() {
+
     }
 
-    // -----------------------------------------------------------------------------------------------------------------
-    //  Actions to be Executed on a Parallel Thread
-    // -----------------------------------------------------------------------------------------------------------------
-
-    public static void runAsync(Runnable runnable){
-        scheduler.execute(() -> {
-            try {
-                runnable.run();
-            }catch (Throwable throwable){
-                throwable.printStackTrace();
-            }
-        });
-    }
-
-    public static void scheduleAsync(Runnable runnable, long delayMillis){
-        scheduler.schedule(() -> {
-            try {
-                runnable.run();
-            }catch (Throwable throwable){
-                throwable.printStackTrace();
-            }
-        }, delayMillis, TimeUnit.MILLISECONDS);
+    public SynchronizedAction getSynchronizedAction() {
+        return synchronizedAction;
     }
 
     // -----------------------------------------------------------------------------------------------------------------
     //  Actions to be Executed on the World Thread
     // -----------------------------------------------------------------------------------------------------------------
 
-    public static void runSync(World world, Runnable runnable){
+    public void runSync(World world, Runnable runnable){
         world.execute(runnable);
     }
 
-    public static void scheduleSync(World world, Runnable runnable, long delayMillis){
-        scheduler.schedule(() -> {
+    public void scheduleSync(World world, Runnable runnable, long delayMillis){
+        FCScheduler.getScheduler().schedule(() -> {
             world.execute(runnable);
         }, delayMillis, TimeUnit.MILLISECONDS);
     }
 
-    public static void scheduleSyncInTicks(World world, Runnable runnable, long delayTicks){
+    public void scheduleSyncInTicks(World world, Runnable runnable, long delayTicks){
         FCScheduler.runAsync(() -> {
             try {
                 Thread.sleep(delayTicks * 50); //TODO Make this respect ticks rather than just wait some random calculated value
@@ -83,7 +53,7 @@ public class FCScheduler {
 
     public static class SynchronizedAction {
 
-        public static <T> T runAndGet(World world, Callable<T> callable){
+        public <T> T runAndGet(World world, Callable<T> callable){
             if (world.isInThread()) {
                 try {
                     return callable.call();
@@ -103,7 +73,7 @@ public class FCScheduler {
             }
         }
 
-        public static <T> T scheduleAndGet(World world, Callable<T> callable, int delayTicks){
+        public <T> T scheduleAndGet(World world, Callable<T> callable, int delayTicks){
             if (world.isInThread()) {
                 throw new RejectedExecutionException("You cannot schedule a SynchronizedAction on the World's [" + world.getName() + "] Own Thread!");
             }
@@ -129,7 +99,7 @@ public class FCScheduler {
             }
         }
 
-        public static void run(World world, Runnable runnable) {
+        public void run(World world, Runnable runnable) {
             if (world.isInThread()) {
                 runnable.run();
                 return;
@@ -140,7 +110,7 @@ public class FCScheduler {
             }, world).join();
         }
 
-        public static void schedule(World world, Runnable runnable, int delayTicks) {
+        public void schedule(World world, Runnable runnable, int delayTicks) {
             scheduleAndGet(
                     world,
                     () -> {
@@ -152,4 +122,5 @@ public class FCScheduler {
         }
 
     }
+
 }
