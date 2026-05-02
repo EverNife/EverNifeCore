7f3d919173f520cdda8022651c1cdce40f7ecbfa
Pétrus Pradella <petrus_ant@hotmail.com>
2026-05-02 00:53:11 -0300

    Create a contract of the MCGameVecAdapter

---
+++ api-contracts/src/main/java/br/com/finalcraft/evernifecore/minecraft/math/game/adapter/MCGameVecAdapter.java
@@ -0,0 +1,30 @@
+package br.com.finalcraft.evernifecore.minecraft.math.game.adapter;
+
+public class MCGameVecAdapter {
+
+    public static class AdaptBlockPos extends MCGameVecAdapter {
+
+    }
+
+    public static class AdaptBlockPosWorld extends AdaptBlockPos {
+
+    }
+
+    public static class AdaptLocPos extends MCGameVecAdapter {
+
+    }
+
+    public static class AdaptLocPosWorld extends AdaptLocPos {
+
+    }
+
+    public static class AdaptChunkPos extends MCGameVecAdapter {
+
+
+    }
+
+    public static class AdaptChunkPosWorld extends AdaptChunkPos {
+
+    }
+
+}
