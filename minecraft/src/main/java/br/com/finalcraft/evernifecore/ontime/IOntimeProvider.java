b6bb84f3c4ba5e8a98b8bc6b545547f0e8f4497b
Pétrus Pradella <petrus_ant@hotmail.com>
2026-05-06 21:25:08 -0300

    [Minecraft To Common] Remove/Reuse/Refactor Common: Ontime

--- minecraft/src/main/java/br/com/finalcraft/evernifecore/ontime/IOntimeProvider.java
+++
@@ -1,9 +0,0 @@
-package br.com.finalcraft.evernifecore.ontime;
-
-import br.com.finalcraft.evernifecore.config.playerdata.IPlayerData;
-
-public interface IOntimeProvider {
-
-    public long getOntime(IPlayerData playerData);
-
-}
