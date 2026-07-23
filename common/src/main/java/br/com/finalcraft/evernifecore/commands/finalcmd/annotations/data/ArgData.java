a8b15a704972366e246a7708b546082a2f527c1a
Petrus Pradella <petrus_ant@hotmail.com>
2026-07-23 02:11:47 -0300

    Finalize the @FlagArg annotation and its ArgData fields
    showOnUsage is carried through inert for now - the help renderer is
    the one that consumes it.

--- common/src/main/java/br/com/finalcraft/evernifecore/commands/finalcmd/annotations/data/ArgData.java
+++ common/src/main/java/br/com/finalcraft/evernifecore/commands/finalcmd/annotations/data/ArgData.java
@@ -24,6 +24,9 @@ public class ArgData {
     private Class<? extends ArgParser> parser;
     private FCLocaleData[] locales = new FCLocaleData[0];
     private String def = "";
+    private String[] aliases = new String[0];
+    private String permission = "";
+    private boolean showOnUsage = true;
 
     public ArgData(Arg arg) {
         this.name = arg.name();
@@ -44,6 +47,10 @@ public class ArgData {
                 .map(FCLocaleData::new)
                 .collect(Collectors.toList())
                 .toArray(new FCLocaleData[0]);
+        this.def = arg.def();
+        this.aliases = arg.aliases();
+        this.permission = arg.permission();
+        this.showOnUsage = arg.showOnUsage();
     }
 
     public ArgData() {
@@ -56,6 +63,10 @@ public class ArgData {
         this.name = this.name.replace(placeholder, value);
         this.context = this.context.replace(placeholder, value);
         this.def = this.def.replace(placeholder, value);
+        this.permission = this.permission.replace(placeholder, value);
+        for (int i = 0; i < this.aliases.length; i++) {
+            this.aliases[i] = this.aliases[i].replace(placeholder, value);
+        }
         for (FCLocaleData locale : this.locales) {
             locale.replace(placeholder, value);
         }
@@ -66,6 +77,10 @@ public class ArgData {
         this.name = replacer.apply(this.name);
         this.context = replacer.apply(this.context);
         this.def = replacer.apply(this.def);
+        this.permission = replacer.apply(this.permission);
+        for (int i = 0; i < this.aliases.length; i++) {
+            this.aliases[i] = replacer.apply(this.aliases[i]);
+        }
         for (FCLocaleData locale : this.locales) {
             locale.replace(replacer);
         }
