b8d11776d2338d3730af2013a5148c911ce59e5f
Petrus Pradella <petrus_ant@hotmail.com>
2026-07-27 16:50:46 -0300

    Print the detected platform in /svinfo
    A misdetection used to surface only as mangled characters inside rendered
    messages; the boot-time answer now has a place an operator can read.

--- common/src/main/java/br/com/finalcraft/evernifecore/commands/misc/CMDSvInfo.java
+++ common/src/main/java/br/com/finalcraft/evernifecore/commands/misc/CMDSvInfo.java
@@ -3,6 +3,7 @@ package br.com.finalcraft.evernifecore.commands.misc;
 import br.com.finalcraft.evernifecore.EverNifeCore;
 import br.com.finalcraft.evernifecore.api.common.commandsender.FCommandSender;
 import br.com.finalcraft.evernifecore.commands.finalcmd.annotations.FinalCMD;
+import br.com.finalcraft.evernifecore.version.FCPlatformType;
 import br.com.finalcraft.everylibs.version.FCJavaVersion;
 
 public class CMDSvInfo {
@@ -14,6 +15,9 @@ public class CMDSvInfo {
         sender.sendMessage("§a-------- SV_INFO --------");
         sender.sendMessage("");
         sender.sendMessage(" §a - JavaVersion: §e" + FCJavaVersion.getCurrent().getName());
+        //Detected from the classpath, not from the running platform provider: printing it is what
+        //makes a misdetection visible on a real server instead of only in the rendered messages.
+        sender.sendMessage(" §a - Platform: §e" + FCPlatformType.getCurrent().getName());
         sender.sendMessage(" §a - EverNifeCore: " + EverNifeCore.instance.getEcPluginData().getMetaInfo().getVersion());
         sender.sendMessage("");
         sender.sendMessage("§a-------- SV_INFO --------");
